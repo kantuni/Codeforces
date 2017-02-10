d72184b756311a3cfaaeee16f7598adce425e665
kantuni <henrikh.kantuni@gmail.com>
2017-02-10 18:32:49 -0500

    More elegant solution

--- 766A/mahmoud_and_longest_uncommon_subsequence.py
+++ 766A/mahmoud_and_longest_uncommon_subsequence.py
@@ -4,12 +4,7 @@ if __name__ == '__main__':
     a = input()
     b = input()
 
-    if len(a) > len(b):
-        print(len(a))
-    elif len(b) > len(a):
-        print(len(b))
+    if a == b:
+        print(-1)
     else:
-        if a == b:
-            print(-1)
-        else:
-            print(len(a))
+        print(max(len(a), len(b)))
