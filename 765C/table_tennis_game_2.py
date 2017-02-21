e5dc5bf03aaf9a72ac9d96226b75fa02f1e3adee
kantuni <henrikh.kantuni@gmail.com>
2017-02-20 23:48:18 -0500

    Solve 765C

--- 765C/table_tennis_game_2.py
+++ 765C/table_tennis_game_2.py
@@ -1,18 +1,27 @@
 """ Created by Henrikh Kantuni on 2/14/17 """
 
 if __name__ == '__main__':
-    k, a, b = input().split()
-    k, a, b = int(k), int(a), int(b)
+    k, a, b = [int(x) for x in input().split()]
 
     score_a = 0
     if a >= k:
-        score_a = int(a / k)
+        score_a = a // k
 
     score_b = 0
     if b >= k:
-        score_b = int(b / k)
+        score_b = b // k
 
     if score_a == 0 and score_b == 0:
         print(-1)
+    elif score_a == 0:
+        if b % k == 0:
+            print(score_b)
+        else:
+            print(-1)
+    elif score_b == 0:
+        if a % k == 0:
+            print(score_a)
+        else:
+            print(-1)
     else:
         print(score_a + score_b)
