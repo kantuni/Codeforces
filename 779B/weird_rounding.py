04694eff48523cdad14a8a48a1887b04ba59dd73
kantuni <henrikh.kantuni@gmail.com>
2017-02-26 20:30:04 -0500

    Add 779B

---
+++ 779B/weird_rounding.py
@@ -0,0 +1,21 @@
+""" Created by Henrikh Kantuni on 2/26/17 """
+
+if __name__ == "__main__":
+    n, k = [int(x) for x in input().split()]
+    digits = list(str(n))
+
+    zero_count = digits.count('0')
+    if zero_count < k:
+        print(len(str(n)) - 1)
+        exit()
+
+    count = 0
+    i = len(digits) - 1
+
+    while int(''.join(digits)) % 10 ** k > 0:
+        if digits[i] != '0':
+            del digits[i]
+            count += 1
+        i -= 1
+
+    print(count)
