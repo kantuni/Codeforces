41dd14af078f45367d18a6a42c3a181953f8fb59
kantuni <henrikh.kantuni@gmail.com>
2017-02-27 22:43:05 -0500

    779C - More elegant solution

--- 779C/dishonest_sellers.cpp
+++ 779C/dishonest_sellers.cpp
@@ -1,59 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 typedef pair<int, int> ii;
 typedef vector<int> vi;
 typedef vector<pair<int, int>> vii;
-
-vii merge(vii left, vii right) {
-  vii merged;
-
-  int i = 0, j = 0;
-  int n = left.size() + right.size();
- 
-  while (i < left.size() && j < right.size()) {
-    if (left[i].first < right[j].first) {
-      merged.push_back(left[i]);
-      ++i;
-    } else {
-      merged.push_back(right[j]);
-      ++j;
-    }
-  }
-
-  while (i < left.size()) {
-    merged.push_back(left[i]);
-    ++i;
-  }
-
-  while (j < right.size()) {
-    merged.push_back(right[j]);
-    ++j;
-  }
-
-  return merged;
-}
-
-vii merge_sort(vii a) {
-  if (a.size() < 2) {
-    return a;
-  }
-
-  vii left, right;
-  
-  for (int i = 0; i < a.size() / 2; ++i) {
-    left.push_back(a[i]);
-  }
-
-  for (int i = a.size() / 2; i < a.size(); ++i) {
-    right.push_back(a[i]);
-  }
-
-  left = merge_sort(left);
-  right = merge_sort(right);
-  return merge(left, right);
-}
 
 int main() {
   int n, k, nc;
@@ -82,8 +34,10 @@ int main() {
     ii d = {al[i] - bl[i], i};
     diff.push_back(d);
   }
-
-  diff = merge_sort(diff);
+  
+  sort(begin(diff), end(diff), [](auto &a, auto &b) {
+    return a.first < b.first;
+  });
 
   int total = 0;
   int i = 0, min_index;
