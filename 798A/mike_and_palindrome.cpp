69f8ca25b46f8bbf54c38940893f0307fc7c81d4
kantuni <henrikh.kantuni@gmail.com>
2017-04-22 23:31:42 -0400

    Add 798A and 798B

---
+++ 798A/mike_and_palindrome.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  
+  if (s.length() == 1) {
+    cout << "YES\n";
+    return 0;
+  }
+  
+  bool palindrome = true;
+  int count = 1;
+  for (int i = 0; i < s.length() / 2; ++i) {
+    if (s[i] != s[s.length() - i - 1]) {
+      if (count > 0) {
+        --count;
+      } else {
+        palindrome = false;
+        break;
+      }
+    }
+  }
+  
+  if (s.length() % 2 == 1 && count == 1) {
+    --count;
+  }
+  
+  if (palindrome && count == 0) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
+}
---
+++ 798B/mike_and_strings.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int offset(string a, string b) {
+  int count = 0;
+  while (a.compare(b) != 0) {
+    b = b.substr(1) + b[0];
+    ++count;
+    if (count > b.length()) {
+      return -1;
+    }
+  }
+  return count;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  
+  vector<string> s(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> s[i];
+  }
+  
+  int minimum = 2000000;
+  
+  for (int i = 0; i < n; ++i) {
+    int count = 0;
+    for (int j = 0; j < n; ++j) {
+      if (i != j) {
+        if (offset(s[i], s[j]) == -1) {
+          cout << -1 << "\n";
+          return 0;
+        }
+        count += offset(s[i], s[j]);
+      }
+    }
+    
+    if (count < minimum) {
+      minimum = count;
+    }
+  }
+  
+  cout << minimum << "\n";
+  return 0;
+}
