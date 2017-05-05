4dce41155a39e3da4b017a41159ded229a65dbac
kantuni <henrikh.kantuni@gmail.com>
2017-05-05 09:53:18 -0400

    Add 805A

---
+++ 805A/fake_np.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  long long l, r;
+  cin >> l >> r;
+  
+  if (l == r) {
+    cout << l << "\n";
+  } else {
+    cout << 2 << "\n";
+  }
+  
+  return 0;
+}
