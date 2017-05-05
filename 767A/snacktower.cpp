b99652ab493f03b65a1f857c5d0651d28e191841
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-05-05 10:18:33 -0400

    Update snacktower.cpp

--- 767A/snacktower.cpp
+++ 767A/snacktower.cpp
@@ -1,1 +1,47 @@
-#include <iostream>#include <vector>using namespace std;int main() {  int n, nc;  vector<int> snacks;  cin >> n;  nc = n;  while (nc > 0) {    int snack;    cin >> snack;    snacks.push_back(snack);    --nc;  }    vector<int> indexes(n + 1);  indexes[0] = -1;    for (int i = 1; i < snacks.size(); ++i) {    indexes[snacks[i]] = i;  }    for (int i = 0; i < indexes[n]; ++i) {    cout << endl;  }  cout << snacks[indexes[n]];    int previous = indexes[n];  for (int i = n - 1; i > 0; --i) {    if (indexes[i] < previous) {      cout << " " << snacks[indexes[i]];    } else {      int before = indexes[i] - previous;      while (before > 0) {        cout << endl;        --before;      }      cout << snacks[indexes[i]];      previous = indexes[i];    }  }  cout << endl;    return 0;}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n, nc;
+  vector<int> snacks;
+
+  cin >> n;
+  nc = n;
+  while (nc > 0) {
+    int snack;
+    cin >> snack;
+    snacks.push_back(snack);
+    --nc;
+  }
+  
+  vector<int> indexes(n + 1);
+  indexes[0] = -1;
+  
+  for (int i = 1; i < snacks.size(); ++i) {
+    indexes[snacks[i]] = i;
+  }
+  
+  for (int i = 0; i < indexes[n]; ++i) {
+    cout << endl;
+  }
+  cout << snacks[indexes[n]];
+  
+  int previous = indexes[n];
+  for (int i = n - 1; i > 0; --i) {
+    if (indexes[i] < previous) {
+      cout << " " << snacks[indexes[i]];
+    } else {
+      int before = indexes[i] - previous;
+      while (before > 0) {
+        cout << endl;
+        --before;
+      }
+      cout << snacks[indexes[i]];
+      previous = indexes[i];
+    }
+  }
+  cout << endl;
+  
+  return 0;
+}
