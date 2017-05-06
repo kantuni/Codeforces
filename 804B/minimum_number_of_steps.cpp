67fbf42461100633a0b696c27699127abc3fd861
kantuni <henrikh.kantuni@gmail.com>
2017-05-06 11:01:29 -0400

    DP version - TLE

--- 804B/minimum_number_of_steps.cpp
+++ 804B/minimum_number_of_steps.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <string>
 #include <iterator>
+#include <algorithm>
+#include <unordered_map>
 using namespace std;
 
-typedef unsigned long long ull;
+typedef unsigned long long huge;
 
-int main() {
-  string str;
-  cin >> str;
-  
-  ull counter = 0;
-  auto pos = str.find("ab");
+pair<string, huge> c(string s) {
+  huge counter = 0;
+  auto pos = s.find("ab");
   while (pos != string::npos) {
-    str.replace(pos, 2, "bba");
-    pos = str.find("ab");
+    s.replace(pos, 2, "bba");
+    pos = s.find("ab");
     ++counter;
   }
+  return {s, counter};
+}
+
+int main() {
+  string s;
+  cin >> s;
   
-  cout << str << "\n";
+  huge counter = 0;
+  unordered_map<string, pair<string, huge>> memo;
+  
+  auto pos = s.find("ab");
+  while (pos != string::npos) {
+    string key = s.substr(0, pos + 2);
+    if (memo.find(key) == end(memo)) {
+      memo[key] = c(key);
+    }
+    s = memo[key].first + s.substr(pos + 2);
+    counter += memo[key].second;
+    pos = s.find("ab");
+  }
+    
   cout << counter % 1000000007 << "\n";
   return 0;
 }
