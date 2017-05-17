2d3e9dd85ee73ed54b15063c83569b80feaca16a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-05-16 20:55:18 -0400

    Update minimum_number_of_steps.cpp

--- 804B/minimum_number_of_steps.cpp
+++ 804B/minimum_number_of_steps.cpp
@@ -1,30 +1,14 @@
 #include <iostream>
 #include <string>
-#include <iterator>
-#include <algorithm>
-#include <unordered_map>
 using namespace std;
 
 typedef unsigned long long huge;
-
-pair<string, huge> c(string s) {
-  huge counter = 0;
-  auto pos = s.find("ab");
-  while (pos != string::npos) {
-    s.replace(pos, 2, "bba");
-    pos = s.find("ab");
-    ++counter;
-  }
-  return {s, counter};
-}
 
 int main() {
   string s;
   cin >> s;
   
   huge counter = 0;
-  unordered_map<string, pair<string, huge>> memo;
-  
   auto pos = s.find("ab");
   while (pos != string::npos) {
     // find /(a)*ab/
@@ -38,12 +22,10 @@ int main() {
       }
     }
     
-    string key = s.substr(bpos, epos - bpos);
-    if (memo.find(key) == end(memo)) {
-      memo[key] = c(key);
-    }
-    s = s.substr(0, bpos) + memo[key].first + s.substr(epos);
-    counter += memo[key].second;
+    string ss = s.substr(bpos, epos - bpos);
+    cout << ss << "\n";
+    s = s.substr(0, bpos) + "bba" + s.substr(epos);
+    counter += 1;
     pos = s.find("ab");
   }
 
