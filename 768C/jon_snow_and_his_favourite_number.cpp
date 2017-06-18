bfba3f029aaf15a160bc15037bed673d74049787
kantuni <henrikh.kantuni@gmail.com>
2017-06-19 02:17:30 +0400

    768C - Time limit exceeded

---
+++ 768C/jon_snow_and_his_favourite_number.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  int n, k, x;
+  cin >> n >> k >> x;
+
+  int a;
+  vector<int> strengths(n);
+
+  for (int i = 0; i < n; ++i) {
+    cin >> strengths[i];
+  }
+  
+  for (int i = 0; i < k; ++i) {
+    sort(begin(strengths), end(strengths));
+    for (int j = 0; j < strengths.size(); ++j) {
+      if (j % 2 == 0) {
+        strengths[j] ^= x;
+      }
+    }
+  }
+
+  int min, max;
+  min = max = strengths[0];
+  
+  for (int i = 0; i < strengths.size(); ++i) {
+    if (min > strengths[i]) {
+      min = strengths[i];
+    }
+
+    if (max < strengths[i]) {
+      max = strengths[i];
+    }
+  }
+
+  cout << max << " " << min << endl;
+  return 0;
+}
