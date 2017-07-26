fd7c94e83658b9e8287c8e3c896be5f0aec51be3
kantuni <henrikh.kantuni@gmail.com>
2017-07-27 02:25:40 +0400

    834B - Accepted

---
+++ 834B/the_festive_evening.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  
+  string s;
+  cin >> s;
+  
+  if (k == 26) {
+    cout << "NO" << "\n";
+    return 0;
+  }
+  
+  vector<int> letters(26, 0);
+  vector<int> seen(26, 0);
+  bool unguarded = false;
+  
+  for (int i = 0; i < s.size(); ++i) {
+    int index = s[i] - 65;
+    ++letters[index];
+  }
+  
+  for (int i = 0; i < s.size(); ++i) {
+    char index = s[i] - 65;
+    
+    if (seen[index] == 0) --k;
+    ++seen[index];
+    
+    if (k < 0) {
+      unguarded = true;
+      break;
+    }
+    
+    if (letters[index] == seen[index]) ++k;
+  }
+  
+  if (unguarded) cout << "YES" << "\n";
+  else cout << "NO" << "\n"; 
+  
+  return 0;
+}
