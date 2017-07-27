520fa7ac4c89ffd75b31cab8903fe10c30e60e7a
kantuni <henrikh.kantuni@gmail.com>
2017-07-28 02:20:41 +0400

    820C - Memory limit exceeded

---
+++ 820C/mister_b_and_boring_game.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  int a, b;
+  long l, r;
+  cin >> a >> b >> l >> r;
+  
+  if (l == r) {
+    cout << 1 << "\n";
+    return 0;
+  }
+  
+  string s = "";
+  for (int i = 0; i < a; ++i) {
+    s += 'a' + i;
+  }
+  
+  int turn = 0;
+  while (s.size() < r) {
+    if (turn % 2 == 0) {
+      if (s.size() < l - 1) {
+        for (int i = s.size() - a; i < s.size(); ++i) {
+          s += s[i];
+        }
+      } else {
+        for (int i = 0; i < a; ++i) {
+          s += s[l - 1];
+        }
+      }
+    } else {
+      string suffix = "";
+      for (int i = s.size() - b; i < s.size(); ++i) {
+        suffix += s[i];
+      }
+      
+      vector<bool> lex(26, true);
+      for (int i = 0; i < suffix.size(); ++i) {
+        int l = (int) suffix[i] - 97;
+        lex[l] = false;
+      }
+      
+      string t = "";
+      for (int i = 0; i < lex.size(); ++i) {
+        if (lex[i] && t.size() < a) {
+          t += 'a' + i;
+        }
+      }
+      s += t;
+    }
+    ++turn;
+  }
+  
+  string answer = "";
+  for (int i = l - 1; i < r; ++i) {
+    answer += s[i];
+  }
+  
+  sort(begin(answer), end(answer));
+  answer.erase(unique(begin(answer), end(answer)), end(answer));
+  cout << answer.size() << "\n";
+  return 0;
+}
