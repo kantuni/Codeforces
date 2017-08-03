d81261e67c8c3ede5ab35c2ec58da7dc2ed26629
kantuni <henrikh.kantuni@gmail.com>
2017-08-04 02:16:03 +0400

    821C - Wrong answer

---
+++ 821C/okabe_and_boxes.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+struct Box {
+  int value;
+  bool sorted = false;
+};
+
+int main() {
+  int n;
+  cin >> n;
+  
+  deque<Box> q;
+  int count = 0;
+  int next = 1;
+  
+  for (int i = 0; i < 2 * n; ++i) {
+    string command;
+    cin >> command;
+    
+    if (command == "add") {
+      int x;
+      cin >> x;
+      Box b;
+      b.value = x;
+      q.push_front(b);
+    } else {
+      if (q[0].value == next || q[0].sorted) {
+        ++next;
+        for (auto &item : q) {
+          item.sorted = true;
+        }
+      } else {
+        ++count;
+        for (auto &item : q) {
+          item.sorted = true;
+        }
+      }
+      q.pop_front();
+    }
+  }
+  
+  cout << count << "\n";
+  return 0;
+}
