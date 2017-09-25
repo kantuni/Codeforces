fbb4cfe38606f48c9b1031e4019635c60959d0e1
kantuni <henrikh.kantuni@gmail.com>
2017-09-25 17:06:19 -0400

    864C - WA

---
+++ 864C/bus.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  ll a, b, f, k;
+  cin >> a >> b >> f >> k;
+  
+  // no need to refuel at all
+  if (b >= a * k) {
+    cout << 0 << "\n";
+    return 0;
+  }
+  
+  // impossible
+  if (b < 2 * f || b < 2 * a - 2 * f) {
+    cout << -1 << "\n";
+    return 0;
+  }
+  
+  if (k == 1 && b < a) {
+    cout << 1 << "\n";
+    return 0;
+  }
+  
+  ll bc = b;
+  ll min = 0;
+  
+  for (int i = 0; i < k; ++i) {
+    if (i % 2 == 0) {
+      if (bc < f) {
+        ++min;
+        bc = b - f;
+      }
+      
+      if (bc < a) {
+        ++min;
+        bc = b - (a - f);
+      } else {
+        bc = b - a;
+      }
+    } else {
+      if (bc < a - f) {
+        ++min;
+        bc = b - (a - f);
+      }
+      
+      if (bc < a) {
+        ++min;
+        bc = b - f;
+      } else {
+        bc = b - a;
+      }
+    }
+  }
+  
+  cout << min << "\n";
+  return 0;
+}
