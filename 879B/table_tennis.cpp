8b76481ec03351e69b0097f3a6fada2588a7635d
kantuni <henrikh.kantuni@gmail.com>
2017-10-27 00:20:16 -0400

    879B - Accepted

---
+++ 879B/table_tennis.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  long long k;
+  cin >> n >> k;
+  
+  vector<int> p(n);
+  for (int i = 0; i < n; i++) {
+    cin >> p[i];
+  }
+  
+  int winner;
+  if (k > n) {
+    winner = *max_element(p.begin(), p.end());
+  } else {
+    queue<int> q;
+    for (int i = 0 ; i < n; i++) {
+      q.push(p[i]);
+    }
+    
+    int wins = 0;
+    winner = q.front();
+    q.pop();
+    
+    while (wins != k) {
+      int front = q.front();
+      if (winner > front) {
+        wins++;
+        q.push(front);
+        q.pop();
+      } else {
+        wins = 1;
+        q.push(winner);
+        winner = q.front();
+        q.pop();
+      }
+    }
+  }
+  
+  cout << winner << "\n";
+  return 0;
+}
