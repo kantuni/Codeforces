be81e43983ddb270b33949243b57c8ce1fd1a419
kantuni <henrikh.kantuni@gmail.com>
2017-10-27 00:19:25 -0400

    879A - Accepted

---
+++ 879A/boryas_diagnosis.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  vector<int> s(n), d(n);
+  for (int i = 0; i < n; i++) {
+    cin >> s[i] >> d[i];
+  }
+  
+  int ans = s[0];
+  for (int i = 1; i < n; i++) {
+    int x = 0;
+    while (s[i] + x * d[i] <= ans) {
+      x++;
+    }
+    ans = s[i] + x * d[i];
+  }
+  
+  cout << ans << "\n";
+  return 0;
+}
