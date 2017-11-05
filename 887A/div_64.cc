0c57c8cd5953b47edccc775c3334d076592c0861
kantuni <henrikh.kantuni@gmail.com>
2017-11-04 22:48:10 -0400

    Added Codeforces Round #444

---
+++ 887A/div_64.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  
+  if (s.size() < 7) {
+    cout << "no\n";
+    return 0;
+  }
+  
+  int zeroes = 0, last;
+  for (int i = s.size() - 1; i >= 0; i--) {
+    if (s[i] == '0') {
+      zeroes++;
+      last = i;
+      if (zeroes == 6)
+        break;
+    }
+  }
+
+  if (zeroes < 6) {
+    cout << "no\n";
+    return 0;
+  }
+  
+  for (int i = last; i >= 0; i--) {
+    if (s[i] == '1') {
+      cout << "yes\n";
+      return 0;
+    }
+  }
+  
+  cout << "no\n";
+  return 0;
+}
