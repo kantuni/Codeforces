78ed9ea858bf268194c668174325908e04f43b52
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-11-30 16:33:50 -0500

    Update timofey_and_cubes.cpp

--- 764B/timofey_and_cubes.cpp
+++ 764B/timofey_and_cubes.cpp
@@ -2,28 +2,28 @@
 #include <vector>
 using namespace std;
 
-typedef long long big;
+typedef long long ll;
 
 int main() {
   int n;
-  vector<big> cubes;
+  vector<ll> cubes;
   
   cin >> n;
-  for (int i = 0; i < n; ++i) {
-    big cube;
+  for (int i = 0; i < n; i++) {
+    ll cube;
     cin >> cube;
     cubes.push_back(cube);
   }
 
-  for (int i = 0; i < n / 2; ++i) {
+  for (int i = 0; i < n / 2; i++) {
     if (i % 2 == 0) {
-      big temp = cubes[i];
+      ll temp = cubes[i];
       cubes[i] = cubes[cubes.size() - 1 - i];
       cubes[cubes.size() - 1 - i] = temp;
     }
   }
 
-  for (int k = 0; k < cubes.size(); ++k) {
+  for (int k = 0; k < cubes.size(); k++) {
     cout << cubes[k];
     if (k != cubes.size() - 1) {
       cout << " ";
