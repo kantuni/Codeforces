0fef63c1f46d9ac6a0953be2409ae81582baaae6
kantuni <henrikh.kantuni@gmail.com>
2018-07-20 19:03:01 +0400

    Educational Codeforces Round 46

---
+++ 1000A/codehorses_tshirts.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool custom(string a, string b) {
+  if (a.size() == b.size()) {
+    return a < b;
+  }
+  return a.size() < b.size();
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<string> prev(n);
+  for (int i = 0; i < n; i++) {
+    cin >> prev[i];
+  }
+  vector<string> curr(n);
+  for (int i = 0; i < n; i++) {
+    cin >> curr[i];
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (prev[i] == curr[j]) {
+        prev[i] = "";
+        curr[j] = "";
+        break;
+      }
+    }
+  }
+  sort(prev.begin(), prev.end(), custom);
+  sort(curr.begin(), curr.end(), custom);
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    if (prev[i] != curr[i]) {
+      ans++;
+    }
+  }
+  cout << ans << "\n";
+  return 0;
+}
---
+++ 1000B/light_it_up.cpp
@@ -0,0 +1,48 @@
+// WA
+#include <bits/stdc++.h>
+using namespace std;
+
+long long power(vector<int> a) {
+  long long sum = 0;
+  for (int i = 0; i < a.size() - 1; i++) {
+    if (i % 2 == 0) {
+      sum += a[i + 1] - a[i];
+    }
+  }
+  return sum;
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> a(n + 2, 0);
+  for (int i = 1; i < n + 1; i++) {
+    cin >> a[i];
+  }
+  a[n + 1] = m;
+  int diff = 0, index = 0;
+  for (int i = 0; i < a.size() - 1; i++) {
+    if (a[i + 1] - a[i] > diff) {
+      diff = a[i + 1] - a[i];
+      index = i;
+    }
+  }
+  long long ans = power(a);
+  if (index % 2 == 1) {
+    a.insert(a.begin() + index + 1, a[index] + 1);
+  } else if (index == 0) {
+    a.insert(a.begin() + 1, a[1] - 1);
+  }
+  bool good = true;
+  for (int i = 0; i < a.size() - 1; i++) {
+    if (a[i + 1] <= a[i]) {
+      good = false;
+      break;
+    }
+  }
+  if (good) {
+    ans = max(ans, power(a));
+  }
+  cout << ans << "\n";
+  return 0;
+}
