e062dd439c88f78de09ca22ab0dd607e56b5fc17
kantuni <henrikh.kantuni@gmail.com>
2018-07-21 18:19:22 +0400

    Educational Codeforces Round 47

---
+++ 1009A/game_shopping.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> c(n);
+  for (int i = 0; i < n; i++) {
+    cin >> c[i];
+  }
+  queue<int> q;
+  while (m--) {
+    int a;
+    cin >> a;
+    q.push(a);
+  }
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    if (q.empty()) {
+      break;
+    }
+    int front = q.front();
+    if (front >= c[i]) {
+      q.pop();
+      ans++;
+    }
+  }
+  cout << ans << "\n";
+  return 0;
+}
