6b6c32188612c13d2d7ec49ba3c033fe2d528b04
kantuni <henrikh.kantuni@gmail.com>
2018-07-21 18:28:21 +0400

    Codeforces Round #498 (Div. 3)

---
+++ 1006A/adjacent_replacements.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  for (int i = 0; i < n; i++) {
+    if (a[i] % 2 == 0) {
+      cout << a[i] - 1 << " ";
+    } else {
+      cout << a[i] << " ";
+    }
+  }
+  cout << "\n";
+  return 0;
+}
