4c03caed987c846a1a9fb1438431de73f0ce8a1b
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-22 22:26:00 +0400

    Update timofey_and_cubes.cpp

--- 764B/timofey_and_cubes.cpp
+++ 764B/timofey_and_cubes.cpp
@@ -1,33 +1,20 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
 
 int main() {
   int n;
-  vector<ll> cubes;
-  
   cin >> n;
+  vector<long long> cubes(n);
   for (int i = 0; i < n; i++) {
-    ll cube;
-    cin >> cube;
-    cubes.push_back(cube);
+    cin >> cubes[i];
   }
-
   for (int i = 0; i < n / 2; i++) {
     if (i % 2 == 0) {
-      ll temp = cubes[i];
-      cubes[i] = cubes[cubes.size() - 1 - i];
-      cubes[cubes.size() - 1 - i] = temp;
+      swap(cubes[i], cubes[cubes.size() - 1 - i]);
     }
   }
-
   for (int k = 0; k < cubes.size(); k++) {
-    cout << cubes[k];
-    if (k != cubes.size() - 1) {
-      cout << " ";
-    }
+    cout << cubes[k] << " ";
   }
   cout << "\n";
   return 0;
