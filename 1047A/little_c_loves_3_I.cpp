46afba643244e2f402083c59b19bfd2e3f708083
kantuni <henrikh.kantuni@gmail.com>
2018-09-25 07:42:55 +0400

    Update little_c_loves_3_I.cpp

--- 1047A/little_c_loves_3_I.cpp
+++ 1047A/little_c_loves_3_I.cpp
@@ -4,11 +4,10 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  cout << 1 << " ";
   if (n % 3 == 2) {
-    cout << 2 << " " << n - 3 << "\n";
+    cout << 1 << " " << 2 << " " << n - 3 << "\n";
   } else {
-    cout << 1 << " " << n - 2 << "\n";
+    cout << 1 << " " << 1 << " " << n - 2 << "\n";
   }
   return 0;
 }
