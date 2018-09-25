10ac9037422378bc84b864c4827a32ad0e75d297
kantuni <henrikh.kantuni@gmail.com>
2018-09-25 07:40:37 +0400

    1047B: Accepted

--- 1047B/cover_points.cpp
+++ 1047B/cover_points.cpp
@@ -1,4 +1,3 @@
-// WA
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,30 +5,10 @@ int main() {
   int n;
   cin >> n;
   vector<pair<int, int>> p(n);
-  pair<int, int> mx, my;
-  mx = my = {-1, -1};
-  for (int i = 0; i < n; i++) {
-    cin >> p[i].first >> p[i].second;
-    if (p[i].first > mx.first) {
-      mx = p[i];
-    } else if (p[i].first == mx.first and p[i].second > mx.second) {
-      mx = p[i];
-    }
-    if (p[i].second > my.second) {
-      my = p[i];
-    } else if (p[i].second == my.second and p[i].first > my.first) {
-      my = p[i];
-    }
-  }
   int ans = -1;
   for (int i = 0; i < n; i++) {
-    int x = p[i].first, y = p[i].second;
-    int b = x + y;
-    int mxy = b - mx.first;
-    int myy = b - my.first;
-    if (mxy >= mx.second and myy >= my.second) {
-      ans = (ans == -1) ? b : min(ans, b);
-    }
+    cin >> p[i].first >> p[i].second;
+    ans = max(ans, p[i].first + p[i].second);
   }
   cout << ans << "\n";
   return 0;
