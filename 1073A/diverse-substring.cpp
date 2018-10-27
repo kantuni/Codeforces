5fea4fcd6a0cf6a1aa989278a7c607292290c0b5
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-10-27 09:56:07 -0400

    Update diverse-substring.cpp

--- 1073A/diverse-substring.cpp
+++ 1073A/diverse-substring.cpp
@@ -8,25 +8,12 @@ int main() {
   cin >> s;
   bool found = false;
   string ans;
-  for (int i = 0; !found and i < n; i++) {
-    for (int len = 2; !found and len <= n - i; len++) {
-      string sub = s.substr(i, len);
-      vector<int> freq(26, 0);
-      for (int j = 0; j < sub.size(); j++) {
-        freq[sub[j] - 'a']++;
-      }
-      bool diverse = true;
-      for (int j = 0; j < 26; j++) {
-        if (freq[j] > sub.size() / 2) {
-          diverse = false;
-          break;
-        }
-      }
-      if (diverse) {
-        found = true;
-        ans = sub;
-        break;
-      }
+  for (int i = 0; i < n - 1; i++) {
+    if (s[i] != s[i + 1]) {
+      found = true;
+      ans += s[i];
+      ans += s[i + 1];
+      break;
     }
   }
   if (found) {
