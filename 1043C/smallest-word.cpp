80b7046cd240b41d5c7711a1941c50f6d5590dba
kantuni <henrikh.kantuni@gmail.com>
2018-10-28 21:38:10 -0400

    Smallest Word - TLE

--- 1043C/smallest-word.cpp
+++ 1043C/smallest-word.cpp
@@ -1,26 +1,32 @@
-// MLE
+// TLE
 #include <bits/stdc++.h>
 using namespace std;
 
-map<pair<string, int>, string> memo;
+map<pair<vector<bool>, int>, vector<bool>> memo;
 map<int, int> trace;
 
-string flip(string s, int len) {
-  string prefix = s.substr(0, len);
-  reverse(prefix.begin(), prefix.end());
-  string rest = s.substr(len);
-  return prefix + rest; 
+vector<bool> flip(vector<bool> s, int len) {
+  vector<bool> result;
+  for (int i = len - 1; i > -1; i--) {
+    result.push_back(s[i]);
+  }
+  for (int i = len; i < s.size(); i++) {
+    result.push_back(s[i]);
+  }
+  return result;
 }
 
-string solve(string s, int len) {
+vector<bool> solve(vector<bool> s, int len) {
   if (len > s.size()) {
+    auto p = make_pair(s, len);
+    memo[p] = s;
     return s;
   }
   auto p1 = make_pair(s, len + 1);
   if (!memo.count(p1)) {
     memo[p1] = solve(s, len + 1);
   }
-  string rs = flip(s, len);
+  vector<bool> rs = flip(s, len);
   auto p2 = make_pair(rs, len + 1);
   if (!memo.count(p2)) {
     memo[p2] = solve(rs, len + 1);
@@ -36,7 +42,15 @@ string solve(string s, int len) {
 int main() {
   string s;
   cin >> s;
-  solve(s, 1);
+  vector<bool> sb;
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] == 'a') {
+      sb.push_back(0);
+    } else {
+      sb.push_back(1);
+    }
+  }
+  solve(sb, 1);
   for (auto t: trace) {
     cout << t.second << " ";
   }
