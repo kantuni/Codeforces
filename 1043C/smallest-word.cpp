2858711f437f9cb554cdb1f6a74e6c64707a983c
kantuni <henrikh.kantuni@gmail.com>
2018-10-30 00:17:01 -0400

    Smallest Word - AC

--- 1043C/smallest-word.cpp
+++ 1043C/smallest-word.cpp
@@ -1,35 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<string, string> memo;
-
-string flip(string s, int len) {
-  string prefix = s.substr(0, len);
-  reverse(prefix.begin(), prefix.end());
-  string rest = s.substr(len);
-  return prefix + rest;
-}
-
-void solve(string s, int len) {
-  if (s.size() < len) return;
-  solve(s, len + 1);
-  string rs = flip(s, len);
-  if (s != rs) {
-    string temp = memo[s];
-    temp[len - 1] = '1';
-    memo[rs] = temp;
-    solve(rs, len + 1);
-  }
-}
-
 int main() {
   string s;
   cin >> s;
-  memo[s] = string(s.size(), '0');
-  solve(s, 1);
-  auto min = *memo.begin();
-  for (int i = 0; i < min.second.size(); i++) {
-    cout << min.second[i] << " ";
+  int n = s.size();
+  vector<int> flip(n);
+  for (int i = 0; i < n; i++) {
+    if (i == n - 1) {
+      flip[i] = s[i] == 'a' ? 1 : 0;
+    } else {
+      flip[i] = s[i] != s[i + 1] ? 1 : 0; 
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    cout << flip[i] << " ";
   }
   cout << "\n";
   return 0;
