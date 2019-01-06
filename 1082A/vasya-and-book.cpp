603d15eb346dd0c4a0e257f41320e5ff64ebe84d
kantuni <henrikh.kantuni@gmail.com>
2019-01-06 17:21:06 +0400

    Educational Codeforces Round 55

---
+++ 1082A/vasya-and-book.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, x, y, d;
+    cin >> n >> x >> y >> d;
+    int diff = abs(y - x);
+    int ans = -1;
+    if (diff % d == 0) {
+      ans = diff / d;
+    } else {
+      int d1 = y - 1;
+      int a1 = -1;
+      if (d1 % d == 0) {
+        a1 = ceil(1.0 * x / d) + d1 / d;
+      }
+      int d2 = n - y;
+      int a2 = -1;
+      if (d2 % d == 0) {
+        a2 = ceil(1.0 * (n - x) / d) + d2 / d; 
+      }
+      if (a1 == -1 or a2 == -1) {
+        ans = max(a1, a2);
+      } else {
+        ans = min(a1, a2);
+      }
+    }
+    cout << ans << "\n";
+  }
+  return 0;
+}
---
+++ 1082B/vova-and-trophies.cpp
@@ -0,0 +1,53 @@
+// WA
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  vector<int> medals;
+  int gold = 0, silver = 0;
+  if (s[0] == 'S') {
+    medals.push_back(gold);
+  }
+  for (int i = 0; i < n; i++) {
+    if (s[i] == 'G') {
+      if (silver > 0) {
+        medals.push_back(silver);
+        silver = 0;
+      }
+      gold++;
+    } else {
+      if (gold > 0) {
+        medals.push_back(gold);
+        gold = 0;
+      }
+      silver++;
+    }
+  }
+  if (gold > 0 or silver > 0) {
+    medals.push_back(max(gold, silver));
+  }
+  int ans = 0;
+  if (medals.size() < 3) {
+    ans = medals[0];
+  } else if (medals.size() == 3) {
+    ans = max(medals[0], medals[2]);
+  } else {
+    for (int i = 0; i < medals.size() - 2; i += 2) {
+      if (medals[i] > 0) {
+        ans = max(ans, medals[i] + 1);
+      }
+      ans = max(ans, medals[i + 2] + 1);
+      bool e1 = i - 2 > 0 and medals[i - 2] > 0;
+      bool e2 = i + 4 < medals.size() and medals[i + 4] > 0;
+      if (medals[i] > 0 and (e1 or e2) and medals[i + 1] == 1) {
+        ans = max(ans, medals[i] + 1 + medals[i + 2]);
+      }
+    }
+  }
+  cout << ans << "\n";
+  return 0;
+}
