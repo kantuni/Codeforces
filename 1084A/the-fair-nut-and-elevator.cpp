d4471154941f2e4bc4e2fb7772c3817aed77bc78
kantuni <henrikh.kantuni@gmail.com>
2019-01-06 17:29:13 +0400

    Codeforces Round #526

---
+++ 1084A/the-fair-nut-and-elevator.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define INF (int) 1e9
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  int ans = INF;
+  for (int x = 1; x < n + 1; x++) {
+    int sum = 0;
+    for (int i = 1; i < n + 1; i++) {
+      sum += a[i - 1] * 2 * (abs(x - i) + x + i - 2);
+    }
+    ans = min(ans, sum);
+  }
+  cout << ans << "\n";
+  return 0;
+}
---
+++ 1084B/kvass-and-the-fair-nut.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  long long s;
+  cin >> n >> s;
+  vector<int> v(n);
+  long long sum = 0;
+  for (int i = 0; i < n; i++) {
+    cin >> v[i];
+    sum += v[i];
+  }
+  int ans = -1;
+  if (sum >= s) {  
+    sort(v.begin(), v.end());
+    long long tmp = 0; 
+    for (int i = 1; i < n; i++) {
+      int diff = v[i] - v[0];
+      tmp += diff;
+    }
+    if (tmp < s) {
+      int cnt = ceil(1.0 * (s - tmp) / n);
+      ans = v[0] - cnt;
+    } else {
+      ans = v[0];
+    }
+  }
+  cout << ans << "\n";
+  return 0;
+}
