a2a1c346dfea0e492b9736a0ed20cf6c00b97436
kantuni <henrikh.kantuni@gmail.com>
2019-01-06 17:05:43 +0400

    Codeforces Round #523

---
+++ 1061A/coins.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long n, s;
+  cin >> n >> s;
+  long long ans = s / n;
+  if (s % n > 0) {
+    ans++;
+  }
+  cout << ans << "\n";
+  return 0;
+}
---
+++ 1061B/views-matter.cpp
@@ -0,0 +1,23 @@
+// WA
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> s(n);
+  long long sum = 0;
+  int mx = 1;
+  for (int i = 0; i < n; i++) {
+    cin >> s[i];
+    sum += s[i];
+    mx = max(mx, s[i]);
+  }
+  sort(s.begin(), s.end());
+  int ans = sum - max(mx, n);
+  if (mx >= n and n > 1 and s[1] == 1) {
+    ans--;
+  }
+  cout << ans << "\n";
+  return 0;
+}
