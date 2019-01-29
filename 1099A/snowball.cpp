7231742be002423ee64131a394e6c19c3b7c97f8
kantuni <henrikh.kantuni@gmail.com>
2019-01-29 05:55:53 +0400

    Codeforces Round #530 (Div. 2)

---
+++ 1099A/snowball.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int w, h;
+  cin >> w >> h;
+  int u1, d1, u2, d2;
+  cin >> u1 >> d1 >> u2 >> d2;
+  while (h > 0) {
+    w += h;
+    if (h == d1) {
+      w = max(w - u1, 0);
+    }
+    if (h == d2) {
+      w = max(w - u2, 0);
+    }
+    h--;
+  }
+  cout << w << "\n";
+  return 0;
+}
---
+++ 1099B/squares-and-segments.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int h = sqrt(n);
+  int w = h;
+  if (h * w < n) {
+    h++;
+  }
+  if (h * w < n) {
+    w++;
+  }
+  cout << h + w << "\n";
+  return 0;
+}
