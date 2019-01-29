a2e18f21806ba6b5d307f6ca424bbf15508baf26
kantuni <henrikh.kantuni@gmail.com>
2019-01-29 05:12:58 +0400

    Avito Cool Challenge 2018

---
+++ 1081A/definite-game.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  if (n == 2) {
+    cout << 2 << "\n";
+  } else {
+    cout << 1 << "\n";
+  }
+  return 0;
+}
---
+++ 1081B/farewell-party.cpp
@@ -0,0 +1,53 @@
+// WA
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  map<int, int> memo;
+  long long sum = 0;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    memo[a[i]]++;
+    sum += a[i];
+  }
+  vector<int> b(n);
+  bool possible = false;
+  if (sum == 0) {
+    possible = true;
+    fill(b.begin(), b.end(), 1);
+  } else if (memo.size() == 1) {
+    auto it = memo.begin();
+    if (it->first == n - 1) {
+      possible = true;
+      for (int i = 0; i < n; i++) {
+        b[i] = i + 1;
+      }
+    } else if (n % 2 == 0 and it->first == n / 2 and it->second == n) {
+      possible = true;
+      for (int i = 0; i < n / 2; i++) {
+        b[i] = 1;
+      }
+      for (int i = n / 2; i < n; i++) {
+        b[i] = 2;
+      }
+    }
+  } else if (memo.size() == 2) {
+    auto it1 = memo.begin();
+    auto it2 = next(it1);
+    possible = it1->first == it2->second and it1->second == it2->first;
+    b = a;
+  }
+  if (possible) {
+    cout << "Possible" << "\n";
+    for (int i = 0; i < n; i++) {
+      cout << b[i] << " ";
+    }
+    cout << "\n";
+  } else {
+    cout << "Impossible" << "\n";
+  }
+  return 0;
+}
