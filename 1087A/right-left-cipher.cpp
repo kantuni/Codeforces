afe04522fbf8aeee58ad5a23df265d4d4199c4d1
kantuni <henrikh.kantuni@gmail.com>
2019-01-29 05:21:18 +0400

    Codeforces Round #528 (Div. 2, based on Technocup 2019 Elimination Round 4)

---
+++ 1087A/right-left-cipher.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string t;
+  cin >> t;
+  string s;
+  int l = 0, r = t.size() - 1;
+  if (t.size() % 2 == 0) {
+    s += t[r];
+    r--;
+  }
+  bool turn = true;
+  while (l <= r) {
+    if (turn) {
+      s = t[l] + s;
+      l++;
+    } else {
+      s = t[r] + s;
+      r--;
+    }
+    turn = !turn;
+  }
+  cout << s << "\n";
+  return 0;
+}
---
+++ 1087B/div-times-mod.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  vector<int> ans;
+  for (int i = 1; i < k; i++) {
+    if (n % i == 0) {
+      int x = i;
+      while (true) {
+        if (x / k * (x % k) == n) {
+          ans.push_back(x);
+          break;
+        }
+        x += k;
+      }
+    }
+  }
+  sort(ans.begin(), ans.end());
+  cout << ans[0] << "\n"; 
+  return 0;
+}
