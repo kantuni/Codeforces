8b1518d445f030a62a4e87cda0a7949aac8895fe
kantuni <henrikh.kantuni@gmail.com>
2019-02-09 19:05:37 +0400

    Update magic-stones.cpp

--- 1110E/magic-stones.cpp
+++ 1110E/magic-stones.cpp
@@ -1,37 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<long long> c, t;
+
+bool solve(vector<long long> state, int left) {
+  if (left == 0) return true;
+  bool ans = false;
+  for (int i = 1; i < state.size() - 1; i++) {
+    if (state[i] != t[i]) {
+      long long old = state[i];
+      long long tmp = state[i + 1] + state[i - 1] - state[i];
+      if (tmp == t[i]) {
+        state[i] = tmp;
+        ans = ans or solve(state, left - 1);
+        state[i] = old;
+      }
+    }
+  }
+  return ans;
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   int n;
   cin >> n;
-  vector<long long> c(n), t(n);
+  c.resize(n);
   for (int i = 0; i < n; i++) {
     cin >> c[i];
   }
+  t.resize(n);
   for (int i = 0; i < n; i++) {
     cin >> t[i];
   }
-  bool again = true;
-  while (again) {
-    again = false;
-    for (int i = n - 2; i > 0; i--) {
-      if (c[i] == t[i]) continue;
-      long long tmp = c[i + 1] + c[i - 1] - c[i];
-      if (tmp == t[i]) {
-        again = true;
-        c[i] = tmp;
-      }
-    }
-  }
-  bool possible = true;
-  for (int i = 0; i < n; i++) {
+  int diff = 0;
+  for (int i = 1; i < n - 1; i++) {
     if (c[i] != t[i]) {
-      possible = false;
-      break;
+      diff++;
     }
   }
+  bool possible = c[0] == t[0] and c[n - 1] == t[n - 1] and solve(c, diff);
   if (possible) {
     cout << "Yes" << endl;
   } else {
