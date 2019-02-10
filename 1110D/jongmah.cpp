4ee9845814cd26c8ab5d062511e7fa90f60a4b93
kantuni <henrikh.kantuni@gmail.com>
2019-02-10 22:48:50 +0400

    Jongmah - Wrong answer

--- 1110D/jongmah.cpp
+++ 1110D/jongmah.cpp
@@ -2,38 +2,32 @@
 using namespace std;
 
 vector<int> f;
-map<vector<int>, int> memo;
-int remember(vector<int> state);
-int solve(vector<int> state);
+map<int, map<int, int>> memo;
+int remember(int i, int a, int b, int c);
+int solve(int i, int a, int b, int c);
 
-int remember(vector<int> state) {
-  if (memo.count(state) == 0) {
-    memo[state] = solve(state);
+int remember(int i, int a, int b, int c) {
+  int hsh = 100 * a + 10 * b + c;
+  if (memo[i][hsh] == 0) {
+    memo[i][hsh] = solve(i, a, b, c);
   }
-  return memo[state];
+  return memo[i][hsh];
 }
 
-int solve(vector<int> state) {
-  int m1 = 0, m2 = 0;
-  for (int i = 0; i < state.size() - 2; i++) {
-    bool same = state[i] > 2;
-    if (same) {
-      state[i] -= 3;
-      m1 = 1 + remember(state);
-      state[i] += 3;
-    }
-    bool seq = state[i] > 0 and state[i + 1] > 0 and state[i + 2] > 0;
-    if (seq) {
-      state[i] -= 1;
-      state[i + 1] -= 1;
-      state[i + 2] -= 1;
-      m2 = 1 + remember(state);
-      state[i] += 1;
-      state[i + 1] += 1;
-      state[i + 2] += 1;
-    }
+int solve(int i, int a, int b, int c) {
+  if (i == f.size()) return 0;
+  int m1 = 0, m2 = 0, m3 = 0;
+  bool same = a > 2;
+  if (same) {
+    m1 = 1 + remember(i, a - 3, b, c);
   }
-  return max(m1, m2);
+  bool seq = min({a, b, c}) > 0;
+  if (seq) {
+    m2 = 1 + remember(i, a - 1, b - 1, c - 1);
+  }
+  int next = i + 3 < f.size() ? f[i + 3] : 0;
+  m3 = remember(i + 1, b, c, next);
+  return max({m1, m2, m3});
 }
 
 int main() {
@@ -41,12 +35,17 @@ int main() {
   cin.tie(0);
   int n, m;
   cin >> n >> m;
-  f.resize(m + 1);
+  f.resize(max(3, m + 1));
   for (int i = 0; i < n; i++) {
     int a;
     cin >> a;
     f[a]++;
   }
-  cout << solve(f) << endl;
+  for (auto val: f) {
+    cout << val << " ";
+  }
+  cout << endl;
+  int ans = solve(0, f[0], f[1], f[2]);
+  cout << ans << endl;
   return 0;
 }
