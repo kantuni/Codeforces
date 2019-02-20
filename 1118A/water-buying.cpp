ea3e6ff96d560f36a69ee80618fd8771092235f9
kantuni <henrikh.kantuni@gmail.com>
2019-02-20 13:54:54 +0400

    Codeforces Round #540 (Div. 3)

---
+++ 1118A/water-buying.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int q;
+  cin >> q;
+  while (q--) {
+    long long n, a, b;
+    cin >> n >> a >> b;
+    long long ans = min(n * a, (n / 2) * b + (n % 2) * a);
+    cout << ans << endl;
+  }
+  return 0;
+}
---
+++ 1118B/tanya-and-candies.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    sum += a[i];
+  }
+  int e = 0, o = 0;
+  for (int i = 0; i < n; i++) {
+    if (i % 2 == 0) {
+      e += a[i];
+    } else {
+      o += a[i];
+    }
+  }
+  int ne = 0, no = 0; 
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    if (i % 2 == 0) {
+      ans += (ne + o - no) * 2 == sum - a[i];
+      ne += a[i];
+    } else {
+      ans += (no + e - ne) * 2 == sum - a[i];
+      no += a[i];
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
