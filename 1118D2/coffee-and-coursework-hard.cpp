3a8dc84828ac1a366659ca9050863b4a79237b3e
kantuni <henrikh.kantuni@gmail.com>
2019-03-11 23:42:24 -0400

    Coffee and Coursework (Hard Version)

---
+++ 1118D2/coffee-and-coursework-hard.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> a;
+
+bool possible(int d) {
+  int total = 0;
+  for (int i = 0; i < n; i++) {
+    int next = max(0, a[i] - i / d);
+    total += next;
+    if (total >= m) {
+      break;
+    }
+  }
+  return total >= m;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cin >> n >> m;
+  a.resize(n);
+  int total = 0;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    total += a[i];
+  }
+  if (total < m) {
+    cout << -1 << endl;
+  } else {
+    sort(a.rbegin(), a.rend());
+    int l = 1, h = n;
+    while (l < h) {
+      int m = (l + h) / 2;
+      if (possible(m)) {
+        h = m;
+      } else {
+        l = m + 1;
+      }
+    }
+    cout << l << endl;
+  }
+  return 0;
+}
