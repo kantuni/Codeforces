e28a5035078e80ac9d70b722ea7a6c0fb74b3fa5
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-03-21 15:59:42 -0400

    Update qaq.cpp

--- 894A/qaq.cpp
+++ 894A/qaq.cpp
@@ -4,18 +4,16 @@ using namespace std;
 int main() {
   string s;
   cin >> s;
-  
   int ans = 0;
   for (int i = 0; i < s.size(); i++) {
     for (int j = i + 1; j < s.size(); j++) {
       for (int k = j + 1; k < s.size(); k++) {
-        if (s[i] == 'Q' && s[j] == 'A' && s[k] == 'Q') {
+        if (s[i] == 'Q' and s[j] == 'A' and s[k] == 'Q') {
           ans++;
         }
       }
     }
   }
-  
-  cout << ans << "\n";
+  cout << ans << endl;
   return 0;
 }
