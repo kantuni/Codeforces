e2d36537d27a1cbbae29026b42399a2263cb3e54
kantuni <henrikh.kantuni@gmail.com>
2019-03-26 05:48:49 -0400

    Update letters-rearranging.cpp

--- 1093B/letters-rearranging.cpp
+++ 1093B/letters-rearranging.cpp
@@ -1,15 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool palindrome(string s) {
-  for (int i = 0; i < s.size() / 2; i++) {
-    if (s[i] != s[s.size() - 1 - i]) {
-      return false;
-    }
-  }
-  return true; 
-}
-
 int main() {
   int t;
   cin >> t;
@@ -17,7 +8,7 @@ int main() {
     string s;
     cin >> s;
     sort(s.begin(), s.end());
-    string ans = palindrome(s) ? "-1" : s;
+    string ans = s[0] == s[s.size() - 1] ? "-1" : s;
     cout << ans << endl;
   }
   return 0;
