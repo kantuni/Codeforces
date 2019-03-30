c10fb1acc212e68d7a1cdc5d18832bb0cc0cf049
kantuni <henrikh.kantuni@gmail.com>
2019-03-30 04:20:53 -0400

    Update the-monster.cpp

--- 787A/the-monster.cpp
+++ 787A/the-monster.cpp
@@ -1,17 +1,19 @@
 #include <bits/stdc++.h>
+#define INF (int) 1e9
 using namespace std;
 
 int main() {
   int a, b, c, d;
   cin >> a >> b >> c >> d;
-  int ans = -1;
+  int ans = INF;
   for (int i = 0; i < 101; i++) {
-    int jc = b - d + i * a;
-    if (jc >= 0 and jc % c == 0) {
-      ans = b + i * a;
-      break;
+    for (int j = 0; j < 101; j++) {
+      if (b + i * a == d + j * c) {
+        ans = min(ans, b + i * a);
+      }
     }
   }
+  ans = (ans == INF) ? -1 : ans;
   cout << ans << endl;
   return 0;
 }
