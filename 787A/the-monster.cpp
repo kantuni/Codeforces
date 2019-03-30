ca39ca2ca2db49b35b8be35c75cd4d2d7f53f592
kantuni <henrikh.kantuni@gmail.com>
2019-03-30 04:13:18 -0400

    787A: The Monster

---
+++ 787A/the-monster.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  int ans = -1;
+  for (int i = 0; i < 101; i++) {
+    int jc = b - d + i * a;
+    if (jc >= 0 and jc % c == 0) {
+      ans = b + i * a;
+      break;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
