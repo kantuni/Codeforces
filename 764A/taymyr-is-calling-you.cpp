74d9c3bfaf1192d10c5930c0c9e1e9f8146c8670
kantuni <henrikh.kantuni@gmail.com>
2019-03-31 04:06:55 -0400

    Update taymyr-is-calling-you.cpp

--- 764A/taymyr-is-calling-you.cpp
+++ 764A/taymyr-is-calling-you.cpp
@@ -4,16 +4,14 @@ using namespace std;
 int main() {
   int n, m, z;
   cin >> n >> m >> z;
-  set<int> min;
+  set<int> t;
   for (int i = n; i <= z; i += n) {
-    min.insert(i);
+    t.insert(i);
   }
   int ans = 0;
   for (int i = m; i <= z; i += m) {
-    if (min.count(i) > 0) {
-      ans++;
-    }
-    min.insert(i);
+    ans += t.count(i);
+    t.insert(i);
   }
   cout << ans << endl;
   return 0;
