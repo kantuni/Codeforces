f03095605dd01202a899a8d066c2f151be8413f1
kantuni <henrikh.kantuni@gmail.com>
2019-05-16 23:40:43 -0400

    1141C - Polycarp Restores Permutation

--- 1141C/polycarp-restores-permutation.cpp
+++ 1141C/polycarp-restores-permutation.cpp
@@ -1,45 +1,41 @@
 #include <bits/stdc++.h>
-#define INF (int) 1e9
 using namespace std;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  int n;
+  long long n;
   cin >> n;
   vector<int> q(n - 1);
-  int mq = INF, mqi = -1;
   for (int i = 0; i < n - 1; i++) {
     cin >> q[i];
-    if (mq > q[i]) {
-      mq = q[i];
-      mqi = i;
-    }
+  }
+  long long qsum = 0, total = 0;
+  for (int i = n - 2; i > -1; i--) {
+    qsum += q[i];
+    total += qsum;
   }
   vector<int> p(n);
-  p[mqi] = mq > 0 ? 1 : -mq + 1;
-  for (int i = mqi - 1; i > -1; i--) {
+  p[n - 1] = (n * (n + 1) / 2 + total) / n;
+  for (int i = n - 2; i > -1; i--) {
     p[i] = p[i + 1] - q[i];
   }
-  for (int i = mqi + 1; i < n; i++) {
-    p[i] = p[i - 1] + q[i - 1];
-  }
-  vector<int> s(p);
-  sort(s.begin(), s.end());
+  vector<int> sp(p);
+  sort(sp.begin(), sp.end());
   bool ok = true;
   for (int i = 0; i < n; i++) {
-    if (s[i] != i + 1) {
+    if (sp[i] != i + 1) {
       ok = false;
       break;
     }
   }
   if (ok) {
-    for (auto val: p) {
-      cout << val << " ";
+    for (auto pi: p) {
+      cout << pi << " ";
     }
     cout << endl;
   } else {
-    cout << -1 << endl;
+   cout << -1 << endl;
   }
   return 0;
 }
