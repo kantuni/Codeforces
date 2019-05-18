1416745dd0f167354c3502e679cab3ad20177502
kantuni <henrikh.kantuni@gmail.com>
2019-05-17 20:10:05 -0400

    771A - Bear and Friendship Condition

---
+++ 771A/bear-and-friendship-condition.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<vector<int>> g;
+vector<int> color;
+
+int dfs(int u) {
+  color[u] = 1;
+  int cnt = 1;
+  for (auto v: g[u]) {
+    if (color[v] == 0) {
+      cnt += dfs(v);
+    }
+  }
+  return cnt;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cin >> n >> m;
+  g.resize(n);
+  color.resize(n);
+  for (int i = 0; i < m; i++) {
+    int u, v;
+    cin >> u >> v;
+    u--, v--;
+    g[u].push_back(v);
+    g[v].push_back(u);
+  }
+  bool ok = true;
+  long long ec = 0;
+  for (int u = 0; u < n; u++) {
+    long long uc = dfs(u);
+    ec += uc * (uc - 1) / 2;
+  }
+  if (ok and ec == m) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+  return 0;
+}
