9e300be8eaef3d1aae35692fa30ac8bcbd8469b5
kantuni <henrikh.kantuni@gmail.com>
2019-05-18 11:49:26 -0400

    864D - Make a Permutation

---
+++ 864D/make-a-permutation.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  map<int, int> memo;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    memo[a[i]]++;
+  }
+  queue<int> q;
+  for (int i = 1; i <= n; i++) {
+    if (memo[i] == 0) {
+      q.push(i);
+    }
+  }
+  int ans = q.size();
+  vector<int> p(n);
+  for (int i = 0; i < n; i++) {
+    if (memo[a[i]] == 0) {
+      p[i] = q.front(); q.pop();
+    } else if (memo[a[i]] == 1) {
+      p[i] = a[i];
+    } else {
+      if (a[i] > q.front()) {
+        p[i] = q.front(); q.pop();
+        memo[a[i]]--;
+      } else {
+        p[i] = a[i];
+        memo[a[i]] = 0;
+      }
+    }
+  }
+  cout << ans << endl;
+  for (auto pi: p) {
+    cout << pi << " ";
+  }
+  cout << endl;
+  return 0;
+}
