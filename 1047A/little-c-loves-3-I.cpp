fa51de1477780feae8cc937cbd49c2b5e48c0358
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-05-27 18:41:55 -0400

    Update and rename little_c_loves_3_I.cpp to little-c-loves-3-I.cpp

--- 1047A/little-c-loves-3-I.cpp
+++ 1047A/little-c-loves-3-I.cpp
@@ -5,9 +5,9 @@ int main() {
   int n;
   cin >> n;
   if (n % 3 == 2) {
-    cout << 1 << " " << 2 << " " << n - 3 << "\n";
+    cout << 1 << " " << 2 << " " << n - 3 << endl;
   } else {
-    cout << 1 << " " << 1 << " " << n - 2 << "\n";
+    cout << 1 << " " << 1 << " " << n - 2 << endl;
   }
   return 0;
 }
