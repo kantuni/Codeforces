0fa4dda2df60dedbdc29190965f02f6a0b4c767a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-06-03 20:13:00 -0400

    Update and rename sherlock_and_his_girlfriend.py to sherlock-and-his-girlfriend.py

---
+++ 776B/sherlock-and-his-girlfriend.py
@@ -0,0 +1,30 @@
+""" Created by Henrikh Kantuni on 2/23/17 """
+
+import math
+
+n = int(input())
+answer = ""
+if n < 3:
+    print(1)
+else:
+    print(2)
+
+m = n + 2
+numbers = [True] * m
+numbers[0] = False
+numbers[1] = False
+
+for i in range(2, int(math.sqrt(m)) + 1):
+    j = 0
+    while i ** 2 + j * i < m:
+        numbers[i ** 2 + j * i] = False
+        j += 1
+
+for i in range(2, m):
+    if numbers[i]:
+        answer += "1 "
+    else:
+        answer += "2 "
+
+answer = answer[:-1]
+print(answer)
