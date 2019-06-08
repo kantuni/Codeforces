e00a4c5768b39b5cacb0bf8b14e206038c929efe
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-06-08 14:40:36 -0400

    Update and rename if_at_first_you_dont_succeed.cpp to if-at-first-you-dont-succeed.cpp

--- 991A/if-at-first-you-dont-succeed.cpp
+++ 991A/if-at-first-you-dont-succeed.cpp
@@ -5,9 +5,9 @@ int main() {
   int a, b, c, n;
   cin >> a >> b >> c >> n;
   if (c > a or c > b or a + b - c + 1 > n) {
-    cout << -1 << "\n";
+    cout << -1 << endl;
   } else {
-    cout << n - (a + b - c) << "\n";
+    cout << n - (a + b - c) << endl;
   }
   return 0;
 }
