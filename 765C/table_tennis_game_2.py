a225d06d891a0330d16c6c787f5f1a69c4c1f8da
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-06-18 10:55:02 -0400

    Update table_tennis_game_2.py

--- 765C/table_tennis_game_2.py
+++ 765C/table_tennis_game_2.py
@@ -1,27 +1,26 @@
 """ Created by Henrikh Kantuni on 2/14/17 """
 
-if __name__ == "__main__":
-    k, a, b = [int(x) for x in input().split()]
+k, a, b = [int(x) for x in input().split()]
 
-    score_a = 0
-    if a >= k:
-        score_a = a // k
+score_a = 0
+if a >= k:
+    score_a = a // k
 
-    score_b = 0
-    if b >= k:
-        score_b = b // k
+score_b = 0
+if b >= k:
+    score_b = b // k
 
-    if score_a == 0 and score_b == 0:
-        print(-1)
-    elif score_a == 0:
-        if b % k == 0:
-            print(score_b)
-        else:
-            print(-1)
-    elif score_b == 0:
-        if a % k == 0:
-            print(score_a)
-        else:
-            print(-1)
+if score_a == 0 and score_b == 0:
+    print(-1)
+elif score_a == 0:
+    if b % k == 0:
+        print(score_b)
     else:
-        print(score_a + score_b)
+        print(-1)
+elif score_b == 0:
+    if a % k == 0:
+        print(score_a)
+    else:
+        print(-1)
+else:
+    print(score_a + score_b)
