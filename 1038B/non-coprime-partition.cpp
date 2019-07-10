b17109f92b8fa0dac52c434d7942d5967b2e9b34
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-07-10 18:17:05 -0400

    Update and rename non_coprime_partition.cpp to non-coprime-partition.cpp

--- 1038B/non-coprime-partition.cpp
+++ 1038B/non-coprime-partition.cpp
@@ -9,7 +9,7 @@ int main() {
   int n;
   cin >> n;
   if (n == 1 or n == 2) {
-    cout << "No" << "\n";
+    cout << "No" << endl;
   } else {
     vector<int> s1, s2;
     long long sum1 = 0, sum2 = 0;
@@ -23,19 +23,19 @@ int main() {
       }
     }
     if (gcd(sum1, sum2) > 1) {
-      cout << "Yes" << "\n";
+      cout << "Yes" << endl;
       cout << s1.size() << " "; 
       for (int i = 0; i < s1.size(); i++) {
         cout << s1[i] << " ";
       }
-      cout << "\n";
+      cout << endl;
       cout << s2.size() << " "; 
       for (int i = 0; i < s2.size(); i++) {
         cout << s2[i] << " ";
       }
-      cout << "\n";
+      cout << endl;
     } else {
-      cout << "No" << "\n";
+      cout << "No" << endl;
     }
   }
   return 0;
