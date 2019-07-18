6ce835f6590bb81cb7a35d3c4019d9dc61992057
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-07-18 12:32:52 -0400

    Update and rename the_rank.cpp to the-rank.cpp

--- 1017A/the-rank.cpp
+++ 1017A/the-rank.cpp
@@ -4,20 +4,20 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  vector<pair<int, int>> students(n);
+  vector<pair<int, int>> s(n);
   int tom = 0;
   for (int i = 0; i < n; i++) {
     int a, b, c, d;
     cin >> a >> b >> c >> d;
-    students[i] = {a + b + c + d, i + 1};
+    s[i] = {a + b + c + d, i + 1};
     if (i == 0) {
       tom = a + b + c + d;
     }
   }
-  sort(students.rbegin(), students.rend());
+  sort(s.rbegin(), s.rend());
   for (int i = 0; i < n; i++) {
-    if (students[i].first == tom) {
-      cout << i + 1 << "\n";
+    if (s[i].first == tom) {
+      cout << i + 1 << endl;
       break;
     }
   }
