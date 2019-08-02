880cd58204b7cbae4bd9d14ef0ceec824ac977f4
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-08-02 02:58:14 -0400

    Update and rename beautiful_divisors.cpp to beautiful-divisors.cpp

--- 893B/beautiful-divisors.cpp
+++ 893B/beautiful-divisors.cpp
@@ -4,16 +4,14 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  
   int k = 1, m = 0, ans = 1;
   while (m <= n) {
-    if (m != 0 && n % m == 0) {
+    if (m != 0 and n % m == 0) {
       ans = m;
     }
     m = (pow(2, k) - 1) * pow(2, k - 1);
     k++;
   }
-  
-  cout << ans << "\n";
+  cout << ans << endl;
   return 0;
 }
