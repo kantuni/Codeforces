2f7526914a8b1b332658d752a093c4aaca3c57cb
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-08-03 20:40:03 -0400

    Update lcm.cpp

--- 1068B/lcm.cpp
+++ 1068B/lcm.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
   long long b;
   cin >> b;
-  set<int> s;
-  for (int i = 1; i < sqrt(b) + 1; i++) {
+  set<long long> s;
+  for (long long i = 1; i < sqrt(b) + 1; i++) {
     if (b % i == 0) {
       s.insert(i);
       s.insert(b / i);
     }
   }
-  cout << s.size() << "\n";
+  cout << s.size() << endl;
   return 0;
 }
