c0ad80a735d88e7738906599bfce8ac5b32e1118
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-08-07 13:47:32 -0400

    Update lost-array.cpp

--- 1043B/lost-array.cpp
+++ 1043B/lost-array.cpp
@@ -6,14 +6,14 @@ int main() {
   cin >> n;
   vector<int> a(n + 1);
   a[0] = 0;
-  for (int i = 1; i <= n; i++) {
+  for (int i = 1; i < n + 1; i++) {
     cin >> a[i];
   }
   set<int> s;
-  for (int k = 1; k <= n; k++) {
+  for (int k = 1; k < n + 1; k++) {
     map<int, int> memo;
     bool ok = true;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i < n + 1; i++) {
       int index = (i - 1) % k;
       if (memo.count(index) and memo[index] != a[i] - a[i - 1]) {
         ok = false;
@@ -25,10 +25,10 @@ int main() {
       s.insert(k);
     }
   }
-  cout << s.size() << "\n";
-  for (auto size: s) {
-    cout << size << " ";
+  cout << s.size() << endl;
+  for (auto val: s) {
+    cout << val << " ";
   }
-  cout << "\n";
+  cout << endl;
   return 0;
 }
