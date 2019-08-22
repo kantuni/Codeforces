9803aeb0a4724e41eebcf3e5407c5a7a4fd6fac6
kantuni <henrikh.kantuni@gmail.com>
2019-08-22 01:23:00 -0400

    Elegant solution

--- 1204A/bowwow-and-the-timetable.cpp
+++ 1204A/bowwow-and-the-timetable.cpp
@@ -6,21 +6,8 @@ int main() {
   cin.tie(nullptr);
   string s;
   cin >> s;
-  if (s.size() == 1) {
-    cout << 0 << endl;
-  } else {
-    string before = "1";
-    int ans = 1;
-    while (before.size() < s.size()) {
-      before += "00";
-      ans++;
-    }
-    if (before.size() == s.size()) {
-      ans = (before == s) ? ans - 1 : ans;
-    } else {
-      ans--;
-    }
-    cout << ans << endl;
-  }
+  bool pot = s.find('1', 1) == string::npos;
+  int ans = pot ? s.size() / 2 : (s.size() + 1) / 2;
+  cout << ans << endl;
   return 0;
 }
