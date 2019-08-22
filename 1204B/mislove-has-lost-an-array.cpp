c180c621cf5d56a658ce8366da465cbaf9abc9f0
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-08-22 00:42:59 -0400

    Update mislove-has-lost-an-array.cpp

--- 1204B/mislove-has-lost-an-array.cpp
+++ 1204B/mislove-has-lost-an-array.cpp
@@ -7,7 +7,7 @@ int main() {
   long long n, l, r;
   cin >> n >> l >> r;
   long long mns = (1 << l) - 1 + (n - l);
-  long long mxs = (1 << r) - 1 + (n - r) * (1 << (r - 1));
+  long long mxs = (1 << r) - 1 + (n - r) * (1 << r >> 1);
   cout << mns << " " << mxs << endl;
   return 0;
 }
