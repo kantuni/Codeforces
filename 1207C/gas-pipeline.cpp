ecdbd373845402677bf566576bbde15d47fb07aa
kantuni <henrikh.kantuni@gmail.com>
2019-08-23 21:22:26 -0400

    Update gas-pipeline.cpp

--- 1207C/gas-pipeline.cpp
+++ 1207C/gas-pipeline.cpp
@@ -1,43 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 vector<vector<long long>> memo;
 long long n, a, b;
 string s;
 
-long long remember(int i, int h);
 long long solve(int i, int h);
-
-long long remember(int i, int h) {
-  if (memo[i][h - 1] == -1) {
-    memo[i][h - 1] = solve(i, h);
-  }
-  return memo[i][h - 1];
-}
+long long remember(int i, int h);
 
 long long solve(int i, int h) {
   if (i == n - 1) {
     return h == 2 ? 2 * a + b : a + b;
   }
-  if (h == 1) {
-    if (s[i + 1] == '0') {
+  if (s[i + 1] == '0') {
+    if (h == 1) {
       long long same = a + b + remember(i + 1, 1);
       long long up = 2 * a + b + remember(i + 1, 2);
       return min(same, up);
     } else {
-      return 2 * (a + b) + remember(i + 1, 2);
+      long long same = a + 2 * b + remember(i + 1, 2);
+      long long down = 2 * a + 2 * b + remember(i + 1, 1);
+      return min(same, down);
     }
   } else {
-    if (s[i + 1] == '0') {
-      long long same = a + 2 * b + remember(i + 1, 2);
-      long long down = 2 * (a + b) + remember(i + 1, 1);
-      return min(same, down);
-    } else {
-      return a + 2 * b + remember(i + 1, 2);
-    }
+    return (2 / h) * a + 2 * b + remember(i + 1, 2);
   }
 }
-
+ 
+long long remember(int i, int h) {
+  if (memo[i][h - 1] == -1) {
+    memo[i][h - 1] = solve(i, h);
+  }
+  return memo[i][h - 1];
+} 
+ 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
