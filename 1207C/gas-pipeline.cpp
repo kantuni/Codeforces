9c9f5099a63e399f38e81bb25391203b876e1cc4
kantuni <henrikh.kantuni@gmail.com>
2019-08-23 22:29:48 -0400

    Elegant solution

--- 1207C/gas-pipeline.cpp
+++ 1207C/gas-pipeline.cpp
@@ -1,49 +1,31 @@
 #include <bits/stdc++.h>
+#define INF (long long) 1e18
 using namespace std;
- 
-vector<vector<long long>> memo;
-long long n, a, b;
-string s;
 
-long long solve(int i, int h);
-long long remember(int i, int h);
-
-long long solve(int i, int h) {
-  if (i == n - 1) {
-    return h == 2 ? 2 * a + b : a + b;
-  }
-  if (s[i + 1] == '0') {
-    if (h == 1) {
-      long long same = a + b + remember(i + 1, 1);
-      long long up = 2 * a + b + remember(i + 1, 2);
-      return min(same, up);
-    } else {
-      long long same = a + 2 * b + remember(i + 1, 2);
-      long long down = 2 * a + 2 * b + remember(i + 1, 1);
-      return min(same, down);
-    }
-  } else {
-    return (2 / h) * a + 2 * b + remember(i + 1, 2);
-  }
-}
- 
-long long remember(int i, int h) {
-  if (memo[i][h - 1] == -1) {
-    memo[i][h - 1] = solve(i, h);
-  }
-  return memo[i][h - 1];
-} 
- 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int t;
   cin >> t;
   while (t--) {
-    cin >> n >> a >> b >> s;
-    memo.assign(n, vector<long long>(2, -1));
-    long long ans = solve(-1, 1) - a;
-    cout << ans << endl;
+    long long n, a, b;
+    cin >> n >> a >> b;
+    string s;
+    cin >> s;
+    long long memo[n + 1][2];
+    for (int i = 0; i < n + 1; i++) {
+      memo[i][0] = memo[i][1] = INF;
+    }
+    memo[0][0] = b;
+    for (int i = 0; i < n; i++) {
+      if (s[i] == '0') {
+        memo[i + 1][0] = min(memo[i][0] + a + b, memo[i][1] + 2 * a + b);
+        memo[i + 1][1] = min(memo[i][1] + a + 2 * b, memo[i][0] + 2 * a + 2 * b);
+      } else {
+        memo[i + 1][1] = memo[i][1] + a + 2 * b;
+      }
+    }
+    cout << memo[n][0] << endl;
   }
   return 0;
 }
