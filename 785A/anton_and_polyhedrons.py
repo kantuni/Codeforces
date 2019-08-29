12ac2bc2f50db3dec3708a9c19d0c0348958cd7b
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-08-29 10:57:12 -0400

    Update anton_and_polyhedrons.py

--- 785A/anton_and_polyhedrons.py
+++ 785A/anton_and_polyhedrons.py
@@ -1,19 +1,16 @@
-""" Created by Henrikh Kantuni on 3/15/17 """
+n = int(input())
 
-if __name__ == "__main__":
-    n = int(input())
+total = 0
+faces = {
+    "Tetrahedron": 4,
+    "Cube": 6,
+    "Octahedron": 8,
+    "Dodecahedron": 12,
+    "Icosahedron": 20,
+}
 
-    total = 0
-    faces = {
-        "Tetrahedron": 4,
-        "Cube": 6,
-        "Octahedron": 8,
-        "Dodecahedron": 12,
-        "Icosahedron": 20,
-    }
+while n > 0:
+    total += faces[input()]
+    n -= 1
 
-    while n > 0:
-        total += faces[input()]
-        n -= 1
-
-    print(total)
+print(total)
