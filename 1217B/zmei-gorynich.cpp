0e1df200d856b793e5c33b452d96dbec09272d10
kantuni <henrikh.kantuni@gmail.com>
2019-09-07 02:12:56 -0400

    Zmei Gorynich

--- 1217B/zmei-gorynich.cpp
+++ 1217B/zmei-gorynich.cpp
@@ -1,4 +1,3 @@
-// WA
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -10,11 +9,16 @@ int main() {
   while (t--) {
     long long n, x;
     cin >> n >> x;
-    vector<long long> d(n);
-    vector<long long> h(n);
+    vector<long long> d(n), h(n);
     for (int i = 0; i < n; i++) {
       cin >> d[i] >> h[i];
     }
+    long long mxd = 0, delta = 0;
+    for (int i = 0; i < n; i++) {
+      mxd = max(mxd, d[i]);
+      delta = max(delta, d[i] - h[i]);
+    }
+    long long ans = mxd >= x ? 1 : -1;
     bool possible = false;
     for (int i = 0; i < n; i++) {
       if (d[i] > h[i]) {  
@@ -22,17 +26,10 @@ int main() {
         break;
       }
     }
-    if (possible) {
-      long long mxd = 0, delta = 0;
-      for (int i = 0; i < n; i++) {
-        mxd = max(mxd, d[i]);
-        delta = max(delta, d[i] - h[i]);
-      }
-      long long ans = ceil((1.0 * x - mxd) / delta) + 1;
-      cout << ans << endl;
-    } else {
-      cout << -1 << endl;
+    if (possible and delta > 0) {
+      ans = ceil(1.0 * max(0LL, x - mxd) / delta) + 1;
     }
+    cout << ans << endl;
   }
   return 0;
 }
