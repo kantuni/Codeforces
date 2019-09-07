7a41df74cfb0aa792fdcbf01c6d428e03128b512
kantuni <henrikh.kantuni@gmail.com>
2019-09-07 12:06:18 -0400

    Update the-number-of-good-substrings.cpp

--- 1217C/the-number-of-good-substrings.cpp
+++ 1217C/the-number-of-good-substrings.cpp
@@ -9,20 +9,21 @@ int main() {
   while (t--) {
     string s;
     cin >> s;
-    vector<int> zb(s.size());
+    int n = s.size();
+    vector<int> zbefore(n);
     int z = 0;
-    for (int i = 0; i < s.size(); i++) {
-      zb[i] = z;
+    for (int i = 0; i < n; i++) {
+      zbefore[i] = z;
       z = s[i] == '0' ? z + 1 : 0;
     }
     long long ans = 0;
-    for (int i = 0; i < s.size(); i++) {
+    for (int i = 0; i < n; i++) {
       if (s[i] == '1') {
         long long num = 0;
         int j = i;
-        while (j < s.size()) {
+        while (j < n) {
           num = 2 * num + (s[j] - '0');
-          if (j - i + 1 + zb[i] < num) {
+          if (j - i + 1 + zbefore[i] < num) {
             break;
           }
           ans++;
