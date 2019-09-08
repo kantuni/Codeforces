fe418f8de56163a1f659a2cbc1e6c556672929a1
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-08 00:21:07 -0400

    Update the-meaningless-game.cpp

--- 834C/the-meaningless-game.cpp
+++ 834C/the-meaningless-game.cpp
@@ -2,13 +2,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef unsigned long long ull;
-
 int main() {
   int n;
   cin >> n;
   for (int i = 0; i < n; i++) {
-    ull a, b;
+    long long a, b;
     cin >> a >> b;
     if (a == 1 and b == 1) {
       cout << "Yes" << endl;
@@ -18,8 +16,8 @@ int main() {
       cout << "Yes" << endl;
       continue;
     }
-    ull m = a * b;
-    ull base = round(cbrt(m));
+    long long m = a * b;
+    long long base = round(cbrt(m));
     bool perfect_cube = false;
     if (m % base == 0) {
       m /= base;
