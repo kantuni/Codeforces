eb1f0f0311c04a0c71f18e38fcc81d701b617f6d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-08 00:20:22 -0400

    Update and rename the_meaningless_game.cpp to the-meaningless-game.cpp

--- 834C/the-meaningless-game.cpp
+++ 834C/the-meaningless-game.cpp
@@ -1,6 +1,5 @@
 // WA
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef unsigned long long ull;
@@ -8,24 +7,19 @@ typedef unsigned long long ull;
 int main() {
   int n;
   cin >> n;
-  
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; i++) {
     ull a, b;
     cin >> a >> b;
-    
-    if (a == 1 && b == 1) {
-      cout << "Yes" << "\n";
+    if (a == 1 and b == 1) {
+      cout << "Yes" << endl;
       continue;
     }
-    
-    if (a == b * b || b == a * a) {
-      cout << "Yes" << "\n";
+    if (a == b * b or b == a * a) {
+      cout << "Yes" << endl;
       continue;
     }
-    
     ull m = a * b;
     ull base = round(cbrt(m));
-
     bool perfect_cube = false;
     if (m % base == 0) {
       m /= base;
@@ -39,13 +33,11 @@ int main() {
         }
       }
     }
-    
     if (perfect_cube) {
-      cout << "Yes" << "\n";
+      cout << "Yes" << endl;
     } else {
-      cout << "No" << "\n";
+      cout << "No" << endl;
     }
   }
-  
   return 0;
 }
