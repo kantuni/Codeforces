0d789bdd23414d885e19a37a56abcd0d9cff7a86
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-17 11:41:59 -0400

    Update cycle_in_maze.cpp

--- 769C/cycle_in_maze.cpp
+++ 769C/cycle_in_maze.cpp
@@ -5,78 +5,69 @@ using namespace std;
 int main() {
   int n, m, k;
   cin >> n >> m >> k;
-  
   char l[n][m];
   int ri, rj;
-  
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < m; ++j) {
-      char symbol;
-      cin >> symbol;
-      if (symbol == 'X') {
-        ri = i;
-        rj = j;
+      char c;
+      cin >> c;
+      if (c == 'X') {
+        ri = i, rj = j;
         l[i][j] = '.';
       } else {
-        l[i][j] = symbol;
+        l[i][j] = c;
       }
     }
   }
-  
-  if (k % 2 != 0) {
-    cout << "IMPOSSIBLE" << "\n";
+  if (k % 2 > 0) {
+    cout << "IMPOSSIBLE" << endl;
     return 0;
   }
-  
   int steps = 0;
-  string answer = "";
-  
+  string ans;
   while (steps < k / 2) {
-    if (ri < n && l[ri + 1][rj] == '.') {
+    if (ri < n and l[ri + 1][rj] == '.') {
       // down
-      answer += "D";
-      ++ri;
+      ans += "D";
+      ri++;
     } else if (rj > 0 && l[ri][rj - 1] == '.') {
       // left
-      answer += "L";
-      --rj;
+      ans += "L";
+      rj--;
     } else if (rj < m && l[ri][rj + 1] == '.') {
       // right
-      answer += "R";
-      ++rj;
+      ans += "R";
+      rj++;
     } else if (ri > 0 && l[ri - 1][rj] == '.') {
       // up
-      answer += "U";
-      --ri;
+      ans += "U";
+      ri--;
     } else {
-      cout << "IMPOSSIBLE" << "\n";
+      cout << "IMPOSSIBLE" << endl;
       return 0;
     }
-    
-    ++steps;
+    steps++;
   }
-  
-  for (int i = answer.length() - 1; i >= 0; --i) {
-    switch (answer[i]) {
+  for (int i = ans.length() - 1; i > -1; i--) {
+    switch (ans[i]) {
       case 'U': {
-        answer += "D";
+        ans += "D";
         break;
       }
       case 'R': {
-        answer += "L";
+        ans += "L";
         break;
       }
       case 'D': {
-        answer += "U";
+        ans += "U";
         break;
       }
       case 'L': {
-        answer += "R";
+        ans += "R";
         break;
       }
     }
   }
-  
-  cout << answer << "\n";
+  cout << ans << endl;
   return 0;
 }
