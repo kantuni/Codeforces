45820e234e492cc91f13e15194aebcc647ff99e0
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-17 11:29:33 -0400

    Update and rename convenient_for_everybody.cpp to convenient-for-everybody.cpp

--- 939C/convenient-for-everybody.cpp
+++ 939C/convenient-for-everybody.cpp
@@ -6,12 +6,14 @@ int n, s, f;
 vector<int> a;
 
 int nofp(int h) {
-  int counter = 0;
+  int cnt = 0;
   for (int i = 0; i < n; i++) {
     int local = (h + i) % n == 0 ? h + i : (h + i) % n;
-    if (local >= s and local < f) counter += a[i];
+    if (local >= s and local < f) {
+      cnt += a[i];
+    }
   }
-  return counter;
+  return cnt;
 }
 
 int main() {
@@ -21,21 +23,19 @@ int main() {
     cin >> a[i];
   }
   cin >> s >> f;
-  
-  int max = 0, hour;
+  int mx = 0, hour;
   for (int i = 1; i <= n / 2 + 1; i++) {
     int c1 = nofp(i);
     int c2 = nofp(n - i + 1);
-    if (c1 > max) {
-      max = c1;
+    if (c1 > mx) {
+      mx = c1;
       hour = i;
     }
-    if (c2 > max) {
-      max = c2;
+    if (c2 > mx) {
+      mx = c2;
       hour = n - i + 1;
     }
   }
-  
-  cout << hour << "\n";
+  cout << hour << endl;
   return 0;
 }
