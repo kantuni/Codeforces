b7bfb23cbbc035eb25f5c7be132dda21a8b0430e
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-17 11:30:16 -0400

    Update and rename mister_b_and_book_reading.cpp to mister-b-and-book-reading.cpp

--- 820A/mister-b-and-book-reading.cpp
+++ 820A/mister-b-and-book-reading.cpp
@@ -5,19 +5,16 @@ using namespace std;
 int main() {
   int c, v0, v1, a, l;
   cin >> c >> v0 >> v1 >> a >> l;
-  
   int read = v0;
   int d = 1;
-  
   while (read < c) {
     if (v0 + d * a < v1 - l) {
       read += v0 + d * a - l;
     } else {
       read += v1 - l;
     }
-    ++d;
+    d++;
   }
-  
-  cout << d << "\n";
+  cout << d << endl;
   return 0;
 }
