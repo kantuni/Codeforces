ff9581d223c39ba2937ed9785d07975371f7af28
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-23 03:49:55 -0400

    Update and rename chess_for_three.cpp to chess-for-three.cpp

--- 893A/chess-for-three.cpp
+++ 893A/chess-for-three.cpp
@@ -4,26 +4,25 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  
   int x = 1, y = 2;
   bool ok = true;
-  
   while (n--) {
     int w;
     cin >> w;
-    
     if (!ok) {
       continue;
     }
-    
-    if (w == x || w == y) {
+    if (w == x or w == y) {
       y = 6 - x - y;
       x = w;
     } else {
       ok = false;
     }
   }
-  
-  cout << (ok ? "YES\n" : "NO\n");
+  if (ok) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
   return 0;
 }
