debaa314d857f9df7a4dc265860040e1cc4262b2
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-23 03:52:04 -0400

    Update online_courses_in_bsu.cpp

--- 770C/online_courses_in_bsu.cpp
+++ 770C/online_courses_in_bsu.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> courses;
-unordered_map<int, int> memo;
-vector<int> order;
 int m;
+vector<int> order;
+vector<vector<int>> courses;
+map<int, int> memo;
 
 int c(int cn) {
   if (m > 0) {
@@ -12,13 +12,11 @@ int c(int cn) {
   } else {
     return -1;
   }
-  
   if (courses[cn].size() == 0) {
     memo[cn] = 0;
     order.push_back(cn + 1);
     return 0;
   }
-  
   int total = 0;
   for (int i = 0; i < courses[cn].size(); ++i) {
     int key = courses[cn][i] - 1;
@@ -28,7 +26,6 @@ int c(int cn) {
       total += memo[key];
     }
   }
-  
   memo[cn] = total;
   order.push_back(cn + 1);
   return total;
@@ -38,30 +35,24 @@ int main() {
   int n, k;
   cin >> n >> k;
   m = n + 1;
-  
   vector<int> primal(k);
   for (int i = 0; i < k; ++i) {
     cin >> primal[i];
   }
-  
   for (int i = 0; i < n; ++i) {
     int length;    
     cin >> length;
-    
     vector<int> course(length);
     for (int j = 0; j < length; ++j) {
       cin >> course[j];
     }
-    
     courses.push_back(course);
   }
-  
   for (int i = 0; i < k; ++i) {
     if (memo.find(primal[i] - 1) == end(memo)) {
       c(primal[i] - 1);
     }
   }
-  
   if (m > 0) {
     cout << order.size() << endl;
     for (auto c : order) {
@@ -71,6 +62,5 @@ int main() {
   } else {
     cout << -1 << endl;
   }
-  
   return 0;
 }
