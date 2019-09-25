c0b1477a9c07ae7c3dab1ea94f0ae16cf72cb01c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-25 01:40:20 -0400

    Update and rename the_useless_toy.cpp to the-useless-toy.cpp

---
+++ 834A/the-useless-toy.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  char start, end, current;
+  cin >> start >> end;
+  int n;
+  cin >> n;
+  // clockwise
+  bool cw = false;
+  current = start;
+  for (int i = 0; i < n; i++) {
+    if (current == 'v') {
+      current = '<';
+    } else if (current == '<') {
+      current = '^';
+    } else if (current == '^') {
+      current = '>';
+    } else if (current == '>') {
+      current = 'v';
+    }
+  }
+  if (current == end) {
+    cw = true;
+  }
+  // counter-clockwise
+  bool ccw = false;
+  current = start;
+  for (int i = 0; i < n; i++) {
+    if (current == 'v') {
+      current = '>';
+    } else if (current == '>') {
+      current = '^';
+    } else if (current == '^') {
+      current = '<';
+    } else if (current == '<') {
+      current = 'v';
+    }
+  }
+  if (current == end) {
+    ccw = true;
+  }
+  if (cw and ccw) {
+    cout << "undefined" << endl;
+  } else if (cw) {
+    cout << "cw" << endl;
+  } else if (ccw) {
+    cout << "ccw" << endl;
+  }
+  return 0;
+}
