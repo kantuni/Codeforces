afe1d0aca916c69d487f320d7f5f3d0c3267e4cb
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-26 10:34:11 +0400

    Update and rename ilya_and_the_tree.cpp to ilya-and-the-tree.cpp

--- 842C/ilya-and-the-tree.cpp
+++ 842C/ilya-and-the-tree.cpp
@@ -1,6 +1,5 @@
 // WA
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct Node {
@@ -17,43 +16,35 @@ int best_gcd(vector<int> numbers) {
   int best = numbers[numbers.size() - 1];
   int min = best;
   int min_index = numbers.size() - 1;
-  
-  for (int i = numbers.size() - 1; i >= 0; --i) {
+  for (int i = numbers.size() - 1; i > -1; i--) {
     if (gcd(best, numbers[i]) < min) {
       min = gcd(best, numbers[i]);
       min_index = i;
     }
   }
-  
   if (min_index != numbers.size() - 1) {
     numbers[min_index] = 0;
   }
-  
-  for (int i = 0; i < numbers.size(); ++i) {
+  for (int i = 0; i < numbers.size(); i++) {
     best = gcd(best, numbers[i]);
   }
-  
   return best;
 }
 
 int main() {
   int n;
   cin >> n;
-  
   vector<Node> nodes(n);
   nodes[0].root = true;
-  
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; i++) {
     cin >> nodes[i].weight;
   }
-  
-  for (int i = 0; i < n - 1; ++i) {
+  for (int i = 0; i < n - 1; i++) {
     int s, t;
     cin >> s >> t;
     nodes[t - 1].parent = s - 1;
   }
-  
-  for (int i = 0; i < nodes.size(); ++i) {
+  for (int i = 0; i < nodes.size(); i++) {
     vector<int> path;
     int current = i;
     while (nodes[current].root != true) {
@@ -63,7 +54,6 @@ int main() {
     path.push_back(nodes[current].weight);
     cout << best_gcd(path) << " ";
   }
-  cout << "\n";
-  
+  cout << endl;
   return 0;
 }
