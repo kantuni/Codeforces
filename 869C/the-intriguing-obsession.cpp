9eead5f2d64a336a3dda5fc10acd301062427866
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-29 08:03:53 +0400

    Update and rename the_intriguing_obsession.cpp to the-intriguing-obsession.cpp

--- 869C/the-intriguing-obsession.cpp
+++ 869C/the-intriguing-obsession.cpp
@@ -1,16 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-typedef unsigned long long ull;
+#include <bits/stdc++.h>
 #define P 998244353
 #define N 5050
+using namespace std;
 
-vector<ull> factorial(N);
+vector<long long> factorial(N);
 
-ull pow(ull a, ull n) {
-  if (n == 0) return 1;
+long long pow(long long a, long long n) {
+  if (n == 0) {
+    return 1;
+  }
   if (n % 2 == 0) {
     return pow((a * a) % P, n / 2);
   } else {
@@ -18,10 +16,12 @@ ull pow(ull a, ull n) {
   }
 }
 
-ull c(ull n, ull k) {
-  if (n == k) return 1;
-  ull res = 1;
-  for (ull i = n - k + 1; i <= n; i++) {
+long long c(long long n, long long k) {
+  if (n == k) {
+    return 1;
+  }
+  long long res = 1;
+  for (long long i = n - k + 1; i <= n; i++) {
     res = (res * i) % P;
   }
   return (res * pow(factorial[k], P - 2)) % P;
@@ -32,41 +32,34 @@ int main() {
   for (int i = 0; i < 3; i++) {
     cin >> colors[i];
   }
-  
   sort(colors.begin(), colors.end(), std::greater<>());
   int r = colors[0];
   int b = colors[1];
   int p = colors[2];
-  
   factorial[0] = 1;
   for (int i = 1; i < N; i++) {
     factorial[i] = (i * factorial[i - 1]) % P;
   }
-  
-  ull rb = 1;
+  long long rb = 1;
   for (int i = 0; i < b; i++) {
-    ull temp = (c(r, b - i) * c(b, b - i)) % P;
+    long long temp = (c(r, b - i) * c(b, b - i)) % P;
     temp = (temp * factorial[b - i]) % P;
     rb = (rb + temp) % P;
   }
-  
-  ull rp = 1;
+  long long rp = 1;
   for (int i = 0; i < p; i++) {
-    ull temp = (c(r, p - i) * c(p, p - i)) % P;
+    long long temp = (c(r, p - i) * c(p, p - i)) % P;
     temp = (temp * factorial[p - i]) % P;
     rp = (rp + temp) % P;
   }
-  
-  ull bp = 1;
+  long long bp = 1;
   for (int i = 0; i < p; i++) {
-    ull temp = (c(b, p - i) * c(p, p - i)) % P;
+    long long temp = (c(b, p - i) * c(p, p - i)) % P;
     temp = (temp * factorial[p - i]) % P;
     bp = (bp + temp) % P;
   }
-  
-  ull ans = (rb * rp) % P;
+  long long ans = (rb * rp) % P;
   ans = (ans * bp) % P;
-  cout << ans << "\n";
-  
+  cout << ans << endl;
   return 0;
 }
