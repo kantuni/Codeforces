3a8876c2a3376e97d1d206f81e6740588a40314d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-29 08:10:04 +0400

    Update the-intriguing-obsession.cpp

--- 869C/the-intriguing-obsession.cpp
+++ 869C/the-intriguing-obsession.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-#define P 998244353
-#define N 5050
+#define P 998244353LL
 using namespace std;
 
-vector<long long> factorial(N);
+const int N = 5050;
+vector<long long> f(N);
 
 long long pow(long long a, long long n) {
   if (n == 0) {
@@ -24,7 +24,7 @@ long long c(long long n, long long k) {
   for (long long i = n - k + 1; i <= n; i++) {
     res = (res * i) % P;
   }
-  return (res * pow(factorial[k], P - 2)) % P;
+  return (res * pow(f[k], P - 2)) % P;
 }
 
 int main() {
@@ -32,31 +32,31 @@ int main() {
   for (int i = 0; i < 3; i++) {
     cin >> colors[i];
   }
-  sort(colors.begin(), colors.end(), std::greater<>());
+  sort(colors.rbegin(), colors.rend());
   int r = colors[0];
   int b = colors[1];
   int p = colors[2];
-  factorial[0] = 1;
+  f[0] = 1;
   for (int i = 1; i < N; i++) {
-    factorial[i] = (i * factorial[i - 1]) % P;
+    f[i] = (i * f[i - 1]) % P;
   }
   long long rb = 1;
   for (int i = 0; i < b; i++) {
-    long long temp = (c(r, b - i) * c(b, b - i)) % P;
-    temp = (temp * factorial[b - i]) % P;
-    rb = (rb + temp) % P;
+    long long tmp = (c(r, b - i) * c(b, b - i)) % P;
+    tmp = (tmp * f[b - i]) % P;
+    rb = (rb + tmp) % P;
   }
   long long rp = 1;
   for (int i = 0; i < p; i++) {
-    long long temp = (c(r, p - i) * c(p, p - i)) % P;
-    temp = (temp * factorial[p - i]) % P;
-    rp = (rp + temp) % P;
+    long long tmp = (c(r, p - i) * c(p, p - i)) % P;
+    tmp = (tmp * f[p - i]) % P;
+    rp = (rp + tmp) % P;
   }
   long long bp = 1;
   for (int i = 0; i < p; i++) {
-    long long temp = (c(b, p - i) * c(p, p - i)) % P;
-    temp = (temp * factorial[p - i]) % P;
-    bp = (bp + temp) % P;
+    long long tmp = (c(b, p - i) * c(p, p - i)) % P;
+    tmp = (tmp * f[p - i]) % P;
+    bp = (bp + tmp) % P;
   }
   long long ans = (rb * rp) % P;
   ans = (ans * bp) % P;
