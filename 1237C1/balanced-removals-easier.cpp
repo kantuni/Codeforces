9d6fc2ee9c43eb48571271fff1a8af5341ef487e
kantuni <henrikh.kantuni@gmail.com>
2019-11-03 17:32:07 +0400

    Balanced Removals (Easier) - Accepted

--- 1237C1/balanced-removals-easier.cpp
+++ 1237C1/balanced-removals-easier.cpp
@@ -1,4 +1,3 @@
-// WA
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,6 +5,15 @@ struct point {
   int x, y, z;
   bool used = false;
 };
+
+struct dist {
+  double value;
+  int i, j;
+};
+
+bool inc(dist &a, dist &b) {
+  return tie(a.value, a.i, a.j) < tie(b.value, b.i, b.j);
+}
 
 int main() {
   ios::sync_with_stdio(false);
@@ -16,27 +24,24 @@ int main() {
   for (int i = 0; i < n; i++) {
     cin >> p[i].x >> p[i].y >> p[i].z;
   }
+  vector<dist> d;
   for (int i = 0; i < n; i++) {
-    bool found = false;
-    if (p[i].used) continue;
-    for (int j = 0; !found and j < n; j++) {
-      if (i == j or p[j].used) continue;
-      for (int k = 0; !found and k < n; k++) {
-        if (k == i or k == j or p[k].used) continue;
-        point a = p[i], b = p[j], c = p[k];
-        bool c1 = min(a.x, b.x) <= c.x and c.x <= max(a.x, b.x);
-        bool c2 = min(a.y, b.y) <= c.y and c.y <= max(a.y, b.y);
-        bool c3 = min(a.z, b.z) <= c.z and c.z <= max(a.z, b.z);
-        if (c1 and c2 and c3) {
-          found = true;
-        }
-      }
-      if (!found) {
-        found = true;
-        p[i].used = true;
-        p[j].used = true;
-        cout << i + 1 << " " << j + 1 << endl;
-      }
+    for (int j = i + 1; j < n; j++) {
+      point a = p[i], b = p[j];
+      dist tmp;
+      tmp.value = hypot(a.x - b.x, a.y - b.y, a.z - b.z);
+      tmp.i = i;
+      tmp.j = j;
+      d.push_back(tmp);
+    }
+  }
+  sort(d.begin(), d.end(), inc);
+  for (auto di: d) {
+    auto [value, i, j] = di;
+    if (!p[i].used and !p[j].used) {
+      p[i].used = true;
+      p[j].used = true;
+      cout << i + 1 << " " << j + 1 << endl;
     }
   }
   return 0;
