de742a75fdcec04079472bb4a5b971071b77cae0
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-11-03 16:09:01 +0400

    Update and rename star_sky.cpp to star-sky.cpp

--- 835C/star-sky.cpp
+++ 835C/star-sky.cpp
@@ -1,8 +1,5 @@
 // TLE
-#include <iostream>
-#include <iterator>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef unsigned long long ull;
@@ -25,58 +22,46 @@ struct View {
 int main() {
   int n, q, c;
   cin >> n >> q >> c;
-
   vector<Star> stars;
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; i++) {
     int x, y, s;
     cin >> x >> y >> s;
-    
     Star temp;
     temp.x = x;
     temp.y = y;
-    temp.s = s;
-    
+    temp.s = s; 
     stars.push_back(temp);
   }
-  
   vector<View> views;
-  for (int i = 0; i < q; ++i) {
+  for (int i = 0; i < q; i++) {
     ull t;
     int x1, y1, x2, y2;
     cin >> t >> x1 >> y1 >> x2 >> y2;
-    
     View temp;
     temp.t = t;
     temp.x1 = x1;
     temp.y1 = y1;
     temp.x2 = x2;
     temp.y2 = y2;
-    
     views.push_back(temp);
   }
-  
   // sort stars by X axis
   StarComparator sc;
   sort(begin(stars), end(stars), sc);
-  
-  for (int i = 0; i < views.size(); ++i) {
+  for (int i = 0; i < views.size(); i++) {
     ull total = 0;
-
     auto low = upper_bound(begin(stars), end(stars), views[i].x1,);
     auto high = lower_bound(begin(stars), end(stars), views[i].x2);
-    
-    for (auto j = low; j <= high; ++j) {
+    for (auto j = low; j <= high; j++) {
       bool inside = stars[j - begin(stars)].x >= views[i].x1;
       inside &= stars[j - begin(stars)].y >= views[i].y1;
       inside &= stars[j - begin(stars)].x <= views[i].x2;
       inside &= stars[j - begin(stars)].y <= views[i].y2;
-      
       if (inside) {
         total += (stars[j - begin(stars)].s + views[i].t) % (c + 1);
       }
     }
-    cout << total << "\n";
+    cout << total << endl;
   }
-
   return 0;
 }
