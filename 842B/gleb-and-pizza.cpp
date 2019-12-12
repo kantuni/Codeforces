e6fa674d4c70be84d9cd18b15db60ea10a69bfed
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-12-13 03:16:26 +0400

    Update gleb-and-pizza.cpp

--- 842B/gleb-and-pizza.cpp
+++ 842B/gleb-and-pizza.cpp
@@ -8,42 +8,34 @@ struct Sausage {
 int main() {
   int r, d;
   cin >> r >> d;
-  
   int n;
   cin >> n;
-  
   vector<Sausage> sausages;
   for (int i = 0; i < n; i++) {
     int xi, yi, ri;
     cin >> xi >> yi >> ri;
-    
     Sausage s;
     s.x = xi;
     s.y = yi;
     s.r = ri;
     sausages.push_back(s);
   }
-  
-  int counter = 0;
+  int ans = 0;
   for (int i = 0; i < sausages.size(); i++) {
     Sausage s = sausages[i];
     double R = hypot(s.x, s.y);
-    
     bool outside = true;
     if (R <= r - s.r) {
       outside = false;
     }
-    
     bool main = false;
     if (R < r - d + s.r) {
       main = true;
     }
-    
     if (!outside and !main) {
-      counter++;
+      ans++;
     }
   }
-  
-  cout << counter << endl;
+  cout << ans << endl;
   return 0;
 }
