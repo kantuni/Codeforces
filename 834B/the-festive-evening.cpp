fbc1e1014e8e885d925fd0c089002b3aa02c711b
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-12-16 17:35:53 +0400

    Update the-festive-evening.cpp

--- 834B/the-festive-evening.cpp
+++ 834B/the-festive-evening.cpp
@@ -1,44 +1,40 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
   int n, k;
   cin >> n >> k;
-  
   string s;
   cin >> s;
-  
   if (k == 26) {
-    cout << "NO" << "\n";
+    cout << "NO" << endl;
     return 0;
   }
-  
   vector<int> letters(26, 0);
   vector<int> seen(26, 0);
   bool unguarded = false;
-  
-  for (int i = 0; i < s.size(); ++i) {
+  for (int i = 0; i < s.size(); i++) {
     int index = s[i] - 65;
-    ++letters[index];
+    letters[index]++;
   }
-  
-  for (int i = 0; i < s.size(); ++i) {
+  for (int i = 0; i < s.size(); i++) {
     char index = s[i] - 65;
-    
-    if (seen[index] == 0) --k;
-    ++seen[index];
-    
+    if (seen[index] == 0) {
+      k--;
+    }
+    seen[index]++;
     if (k < 0) {
       unguarded = true;
       break;
     }
-    
-    if (letters[index] == seen[index]) ++k;
+    if (letters[index] == seen[index]) {
+      k++;
+    }
   }
-  
-  if (unguarded) cout << "YES" << "\n";
-  else cout << "NO" << "\n"; 
-  
+  if (unguarded) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
   return 0;
 }
