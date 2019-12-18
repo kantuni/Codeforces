6c2b4019acb004fb0f2a3f04549166baba56eb49
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-12-18 20:36:18 +0400

    Update messages.cpp

--- 964B/messages.cpp
+++ 964B/messages.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
+#define INF (int) 1e9
 using namespace std;
 
 int main() {
   int n, A, B, C, T;
   cin >> n >> A >> B >> C >> T;
   vector<int> t(n);
-  int mint = 1e5;
+  int mnt = INF;
   for (int i = 0; i < n; i++) {
     cin >> t[i];
-    mint = min(mint, t[i]);
+    mnt = min(mnt, t[i]);
   }
-  if (mint > T) {
-    cout << 0 << "\n";
+  if (mnt > T) {
+    cout << 0 << endl;
     return 0;
   }
   vector<int> c(n, A);
@@ -22,6 +23,6 @@ int main() {
     ans2 += A - k * B;
     ans2 += C * k;
   }
-  cout << max(ans1, ans2) << "\n";
+  cout << max(ans1, ans2) << endl;
   return 0;
 }
