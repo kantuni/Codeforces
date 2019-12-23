9ce0f9c1665d92e55806def07b47b0c3393fc737
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-12-23 20:44:51 +0400

    Update key-races.cpp

--- 835A/key-races.cpp
+++ 835A/key-races.cpp
@@ -1,16 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
   int s, v1, v2, t1, t2;
   cin >> s >> v1 >> v2 >> t1 >> t2;
-  
   int p1 = 2 * t1 + v1 * s;
   int p2 = 2 * t2 + v2 * s;
-  
-  if (p1 == p2) cout << "Friendship" << "\n";
-  else if (p1 < p2) cout << "First" << "\n";
-  else cout << "Second" << "\n";
-
+  if (p1 == p2) {
+    cout << "Friendship" << endl;
+  } else if (p1 < p2) {
+    cout << "First" << endl;
+  } else {
+    cout << "Second" << endl;
+  }
   return 0;
 }
