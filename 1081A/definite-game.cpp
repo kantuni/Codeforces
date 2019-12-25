15d3d450a3122bc3bafa25c60cd242089e06dc68
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-12-25 10:27:36 +0400

    Update definite-game.cpp

--- 1081A/definite-game.cpp
+++ 1081A/definite-game.cpp
@@ -4,10 +4,7 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  if (n == 2) {
-    cout << 2 << "\n";
-  } else {
-    cout << 1 << "\n";
-  }
+  int ans = (n == 2) ? 2 : 1;
+  cout << ans << endl;
   return 0;
 }
