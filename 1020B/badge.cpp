a11d5582281d51f4e07089d43301ac09e8d11cc7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-02-04 17:35:24 -0500

    Update badge.cpp

--- 1020B/badge.cpp
+++ 1020B/badge.cpp
@@ -9,14 +9,14 @@ int main() {
     cin >> p[i];
   }
   for (int i = 0; i < n; i++) {
-    vector<int> holes(n, 0);
+    vector<int> h(n, 0);
     int j = i;
-    while (holes[j] < 2) {
-      holes[j]++;
+    while (h[j] < 2) {
+      h[j]++;
       j = p[j] - 1;
     }
     cout << j + 1 << " ";
   }
-  cout << "\n";
+  cout << endl;
   return 0;
 }
