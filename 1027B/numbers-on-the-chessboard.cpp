3198bd8dce084b6a66c15692e3b7a42c44f77128
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-02-05 22:29:30 -0500

    Update numbers-on-the-chessboard.cpp

--- 1027B/numbers-on-the-chessboard.cpp
+++ 1027B/numbers-on-the-chessboard.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-  unsigned long long n, q;
+  long long n, q;
   cin >> n >> q;
   while (q--) {
-    unsigned long long x, y;
+    long long x, y;
     cin >> x >> y;
-    unsigned long long ans = n * (x - 1) + y;
+    long long ans = n * (x - 1) + y;
     ans = (ans + 1) / 2;
-    unsigned long long sum = x + y;
+    long long sum = x + y;
     ans += (sum % 2 == 1) ? (n * n + 1) / 2 : 0;
     cout << ans << endl;
   }
