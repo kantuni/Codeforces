b5d7eb1a81bb0943931fbff51a6c9cc6da720585
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-03-26 14:02:53 +0400

    Update and rename mahmoud_and_ehab_and_the_wrong_algorithm.cpp to mahmoud-and-ehab-and-the-wrong-algorithm.cpp

--- 959C/mahmoud-and-ehab-and-the-wrong-algorithm.cpp
+++ 959C/mahmoud-and-ehab-and-the-wrong-algorithm.cpp
@@ -5,16 +5,16 @@ int main() {
   int n;
   cin >> n;
   if (n < 6) {
-    cout << "-1" << "\n";
+    cout << "-1" << endl;
   } else {
     for (int i = 2; i < n - 1; i++) {
-      cout << "1 " << i << "\n";
+      cout << "1 " << i << endl;
     }
-    cout << "2 " << n - 1 << "\n";
-    cout << "2 " << n << "\n";
+    cout << "2 " << n - 1 << endl;
+    cout << "2 " << n << endl;
   }
   for (int i = 2; i < n + 1; i++) {
-    cout << "1 " << i << "\n";
+    cout << "1 " << i << endl;
   }
   return 0;
 }
