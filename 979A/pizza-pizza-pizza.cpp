6b294596be383d4cd1fbdd746ecff3e52630892d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-03-29 11:35:02 +0400

    Update pizza-pizza-pizza.cpp

--- 979A/pizza-pizza-pizza.cpp
+++ 979A/pizza-pizza-pizza.cpp
@@ -6,7 +6,7 @@ int main() {
   cin >> n;
   if (n == 0) {
     cout << 0 << endl;
-  } else if ((n + 1) % 2) {
+  } else if (n % 2 == 0) {
     cout << n + 1 << endl;
   } else {
     cout << (n + 1) / 2 << endl;
