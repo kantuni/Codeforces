1ee1d06ef142fab75df7f0e44d47dcf023995adc
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-03-31 00:14:45 +0400

    Update mister-b-and-book-reading.cpp

--- 820A/mister-b-and-book-reading.cpp
+++ 820A/mister-b-and-book-reading.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main() {
   int c, v0, v1, a, l;
   cin >> c >> v0 >> v1 >> a >> l;
-  int read = v0;
+  int tmp = v0;
   int d = 1;
-  while (read < c) {
+  while (tmp < c) {
     if (v0 + d * a < v1 - l) {
-      read += v0 + d * a - l;
+      tmp += v0 + d * a - l;
     } else {
-      read += v1 - l;
+      tmp += v1 - l;
     }
     d++;
   }
