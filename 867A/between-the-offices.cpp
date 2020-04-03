4cd3589d0aa69154b7c57bf809f6cf8bc4c6b022
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-03 12:04:25 +0400

    Update between-the-offices.cpp

--- 867A/between-the-offices.cpp
+++ 867A/between-the-offices.cpp
@@ -1,27 +1,25 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {  
   int n;
   cin >> n;
-  
   string s;
   cin >> s;
-  
   int sf = 0, fs = 0;
   for (int i = 1; i < s.size(); i++) {
     if (s[i - 1] != s[i]) {
-      if (s[i - 1] == 'S') sf++;
-      else fs++;
+      if (s[i - 1] == 'S') {
+        sf++;
+      } else {
+        fs++;
+      }
     }
   }
-  
   if (sf > fs) {
-    cout << "YES" << "\n";
+    cout << "YES" << endl;
   } else {
-    cout << "NO" << "\n";
+    cout << "NO" << endl;
   }
-  
   return 0;
 }
