62dfa1f1a03c4e019a23ece84e6d2bf02bf91b8d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-05 16:02:46 +0400

    Update letters-rearranging.cpp

--- 1093B/letters-rearranging.cpp
+++ 1093B/letters-rearranging.cpp
@@ -8,7 +8,7 @@ int main() {
     string s;
     cin >> s;
     sort(s.begin(), s.end());
-    string ans = s[0] == s[s.size() - 1] ? "-1" : s;
+    string ans = s.front() == s.back() ? "-1" : s;
     cout << ans << endl;
   }
   return 0;
