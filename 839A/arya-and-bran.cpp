adcae6be29d2a9e41671dfbf9e3677299e820be8
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-06 15:08:55 +0400

    Update and rename arya_and_bran.cpp to arya-and-bran.cpp

--- 839A/arya-and-bran.cpp
+++ 839A/arya-and-bran.cpp
@@ -4,33 +4,26 @@ using namespace std;
 int main() {
   int n, k;
   cin >> n >> k;
-  
   int arya = 0, bran = 0;
   int mnd = 0;
-  
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; i++) {
     int today;
     cin >> today;
-    
     if (today + arya >= 8) {
       bran += 8;
       arya += today - 8;
-      ++mnd;
     } else {
       bran += today + arya;
       arya = 0;
-      ++mnd;
     }
-    
+    mnd++;
     if (bran >= k) {
-      cout << mnd << "\n";
+      cout << mnd << endl;
       return 0;
     }
   }
-  
   if (bran < k) {
-    cout << -1 << "\n";
+    cout << -1 << endl;
   }
-  
   return 0;
 }
