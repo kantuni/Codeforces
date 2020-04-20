047af0f8a6c2554115b676378e93d5a9e93961dc
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-20 13:55:51 +0400

    Update table-tennis.cpp

--- 879B/table-tennis.cpp
+++ 879B/table-tennis.cpp
@@ -2,38 +2,33 @@
 using namespace std;
 
 int main() {
-  int n;
-  long long k;
+  long long n, k;
   cin >> n >> k;
   vector<int> p(n);
+  int ans = -1;
   for (int i = 0; i < n; i++) {
     cin >> p[i];
+    ans = max(ans, p[i]);
   }
-  int winner;
-  if (k > n) {
-    winner = *max_element(p.begin(), p.end());
-  } else {
+  if (k <= n) {
     queue<int> q;
     for (int i = 0 ; i < n; i++) {
       q.push(p[i]);
     }
     int wins = 0;
-    winner = q.front();
-    q.pop();
-    while (wins != k) {
+    ans = q.front(); q.pop();
+    while (wins < k) {
       int front = q.front();
-      if (winner > front) {
+      if (ans > front) {
+        q.push(front); q.pop();
         wins++;
-        q.push(front);
-        q.pop();
       } else {
+        q.push(ans);
+        ans = q.front(); q.pop();
         wins = 1;
-        q.push(winner);
-        winner = q.front();
-        q.pop();
       }
     }
   }
-  cout << winner << "\n";
+  cout << ans << endl;
   return 0;
 }
