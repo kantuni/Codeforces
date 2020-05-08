d3ca5a034059eb001b68200479370919bf4e31c4
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-05-09 00:09:45 +0400

    Update jamie-and-alarm-snooze.cpp

--- 916A/jamie-and-alarm-snooze.cpp
+++ 916A/jamie-and-alarm-snooze.cpp
@@ -1,24 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
   int x;
   cin >> x;
-  
   int h, m;
   cin >> h >> m;
-  
-  int count = 0;
-  while (h % 10 != 7 && m % 10 != 7) {
+  int ans = 0;
+  while (h % 10 != 7 and m % 10 != 7) {
     if (m < x) {
       m = (m + 60 - x) % 60;
-      h = (h == 0) ? 23 : --h;
+      h = (h == 0) ? 23 : h - 1;
     } else {
       m -= x;
     }
-    count++;
+    ans++;
   }
-  
-  cout << count << "\n";
+  cout << ans << endl;
   return 0;
 }
