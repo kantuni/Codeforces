40d6b8a965a35baa022c9ea80a807413d95ddbb8
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-05-28 17:34:56 +0400

    Update the-bits.cpp

--- 1017B/the-bits.cpp
+++ 1017B/the-bits.cpp
@@ -6,12 +6,12 @@ int main() {
   cin >> n;
   string a, b;
   cin >> a >> b;
-  long long zeroes = 0, ones = 0, oz = 0;
+  long long zeros = 0, ones = 0, oz = 0;
   for (int i = 0; i < n; i++) {
     if (a[i] == '1') {
       ones++;
     } else {
-      zeroes++;
+      zeros++;
     }
     if (a[i] == '1' and b[i] == '0') {
       oz++;
@@ -20,7 +20,7 @@ int main() {
   long long ans = 0;
   for (int i = 0; i < n; i++) {
     if (a[i] == '1' and b[i] == '0') {
-      ans += zeroes;
+      ans += zeros;
     } else if (a[i] == '0' and b[i] == '0') {
       ans += ones - oz;
     }
