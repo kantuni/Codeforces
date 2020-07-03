448f7bc25df9c36d6502760e088fe3f3d34e9bc4
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-07-03 14:32:50 +0400

    Update magic_forest.cpp

--- 922B/magic_forest.cpp
+++ 922B/magic_forest.cpp
@@ -4,15 +4,17 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  int counter = 0;
+  int ans = 0;
   for (int i = 1; i < n - 1; i++) {
     for (int j = i + 1; j < n; j++) {
       for (int k = j + 1; k < i + j and k < n + 1; k++) {
-        int sum = i ^ j ^ k;
-        if (sum == 0) counter++;
+        int sum = i xor j xor k;
+        if (sum == 0) {
+          ans++;
+        }
       }
     }
   }
-  cout << counter << "\n";
+  cout << ans << endl;
   return 0;
 }
