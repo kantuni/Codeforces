a2eb2ed86141ff99551bc4e1496b84eeb00dc0db
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-08-15 01:09:54 -0400

    Update alphabetic-removals.cpp

--- 999C/alphabetic-removals.cpp
+++ 999C/alphabetic-removals.cpp
@@ -8,13 +8,13 @@ int main() {
   cin >> s;
   string ss = s;
   sort(ss.begin(), ss.end());
-  vector<int> abc(26);
+  vector<int> alphabet(26);
   for (int i = 0; k > 0 and i < n; i++, k--) {
-    abc[ss[i] - 'a']++;
+    alphabet[ss[i] - 'a']++;
   }
   for (int i = 0; i < n; i++) {
-    if (abc[s[i] - 'a'] > 0) {
-      abc[s[i] - 'a']--;
+    if (alphabet[s[i] - 'a'] > 0) {
+      alphabet[s[i] - 'a']--;
     } else {
       cout << s[i];
     }
