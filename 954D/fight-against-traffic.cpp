4650ddc522bf4ea8c7f13c798f89591d64ba3358
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-08-15 01:08:36 -0400

    Update fight-against-traffic.cpp

--- 954D/fight-against-traffic.cpp
+++ 954D/fight-against-traffic.cpp
@@ -3,31 +3,28 @@
 #define INF (int) 1e9
 using namespace std;
 
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-
 int main() {
   int n, m, s, t;
   cin >> n >> m >> s >> t; 
   s--; t--;
-  vvi AdjList(n, vi());
+  vector<vector<int>> g(n, vector<int>());
   for (int i = 0; i < m; i++) {
     int u, v;
     cin >> u >> v;
     u--; v--;
-    AdjList[u].push_back(v);
-    AdjList[v].push_back(u);
+    g[u].push_back(v);
+    g[v].push_back(u);
   }
   // calculate dist with BFS
-  vi disto(n, INF);
+  vector<int> disto(n, INF);
   disto[s] = 0;
   queue<int> qo;
   qo.push(s);
   while (!qo.empty()) {
     int u = qo.front();
     qo.pop();
-    for (int k = 0; k < AdjList[u].size(); k++) {
-      int v = AdjList[u][k];
+    for (int k = 0; k < g[u].size(); k++) {
+      int v = g[u][k];
       if (disto[v] == INF) {
         disto[v] = disto[u] + 1;
         qo.push(v);
@@ -37,27 +34,29 @@ int main() {
   int cnt = 0;
   for (int i = 0; i < n; i++) {
     for (int j = i + 1; j < n; j++) {
-      vi elist = AdjList[i];
+      vector<int> elist = g[i];
       if (find(elist.begin(), elist.end(), j) == elist.end()) {
-        AdjList[i].push_back(j);
+        g[i].push_back(j);
         // calculate dist with BFS
-        vi dist(n, INF);
+        vector<int> dist(n, INF);
         dist[s] = 0;
         queue<int> q;
         q.push(s);
         while (!q.empty()) {
           int u = q.front();
           q.pop();
-          for (int k = 0; k < AdjList[u].size(); k++) {
-            int v = AdjList[u][k];
+          for (int k = 0; k < g[u].size(); k++) {
+            int v = g[u][k];
             if (dist[v] == INF) {
               dist[v] = dist[u] + 1;
               q.push(v);
             }
           }
         }
-        if (dist[t] < disto[t]) cnt++;
-        AdjList[i].pop_back();
+        if (dist[t] < disto[t]) {
+          cnt++;
+        }
+        g[i].pop_back();
       }
     }
   }
