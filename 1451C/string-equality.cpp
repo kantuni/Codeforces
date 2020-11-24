2b3a168e719624ac4d926522083cb7ee03c75937
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-11-23 22:32:28 -0500

    String Equality

--- 1451C/string-equality.cpp
+++ 1451C/string-equality.cpp
@@ -1,4 +1,3 @@
-// WA
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,26 +11,23 @@ int main() {
     cin >> n >> k;
     string a, b;
     cin >> a >> b;
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
+    vector<int> ac(26), bc(26);
     for (int i = 0; i < n; i++) {
-      bool inc = true;
-      for (int j = 0; j < k; j++) {
-        bool c1 = a[i] != a[i + j];
-        bool c2 = b[i] != b[i + j];
-        bool c3 = a[i] > b[i];
-        if (c1 or c2 or c3) {
-          inc = false;
-          break;
-        }
+      ac[a[i] - 'a']++;
+      bc[b[i] - 'a']++;
+    }
+    bool ok = true;
+    for (int i = 0; i < 26; i++) {
+      int diff = ac[i] - bc[i];
+      if (diff < 0 or diff % k > 0) {
+        ok = false;
+        break;
       }
-      if (inc) {
-        for (int j = 0; j < k; j++) {
-          a[i + j] = b[i];
-        }
+      if (i < 25) {
+        ac[i + 1] += diff;
       }
     }
-    if (a == b) {
+    if (ok) {
       cout << "Yes" << endl;
     } else {
       cout << "No" << endl;
