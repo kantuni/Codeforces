479a6ec809cb595f9a64398f29e550138fc7bbd2
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-11-25 22:50:58 -0500

    Start with 0s instead of 1s

--- 1352F/binary-string-reconstruction.cpp
+++ 1352F/binary-string-reconstruction.cpp
@@ -10,11 +10,11 @@ int main() {
     int n0, n1, n2;
     cin >> n0 >> n1 >> n2;
     string s;
-    if (n2 > 0) {
-      s += string(n2 + 1, '1');
-    }
     if (n0 > 0) {
       s += string(n0 + 1, '0');  
+    }
+    if (n2 > 0) {
+      s += string(n2 + 1, '1');
     }
     if (n0 > 0 and n2 > 0) {
       n1--;
