18f97536e3f2d758411d8b177622c22940497f91
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-12-07 13:57:20 -0500

    Update dishonest-sellers.cpp

--- 779C/dishonest-sellers.cpp
+++ 779C/dishonest-sellers.cpp
@@ -8,7 +8,6 @@ typedef vector<pair<int, int>> vii;
 int main() {
   int n, k, nc;
   cin >> n >> k;
-
   vi al;
   nc = n;
   while (nc--) {
@@ -16,7 +15,6 @@ int main() {
     cin >> a;
     al.push_back(a);
   }
-
   vi bl;
   nc = n;
   while (nc--) {
@@ -24,20 +22,16 @@ int main() {
     cin >> b;
     bl.push_back(b);
   }
-
   vii diff;
   for (int i = 0; i < n; ++i) {
     ii d = {al[i] - bl[i], i};
     diff.push_back(d);
   }
-  
   sort(begin(diff), end(diff), [](auto &a, auto &b) {
     return a.first < b.first;
   });
-
   int total = 0;
   int i = 0, min_index;
-
   while (k--) {
     min_index = diff[i].second;
     total += al[min_index];
@@ -45,7 +39,6 @@ int main() {
     bl[min_index] = 0;
     i++;
   }
-
   while (i < n and diff[i].first < 0) {
     min_index = diff[i].second;
     total += al[min_index];
@@ -53,11 +46,9 @@ int main() {
     bl[min_index] = 0;
     i++;   
   }
-
   for (auto value : bl) {
     total += value;
   }
-
   cout << total << endl;
   return 0;
 }
