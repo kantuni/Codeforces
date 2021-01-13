a34f6fd4f6aefd6a991be54decb2e8b648fc458e
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-01-13 05:11:02 +0400

    769C: TLE

--- 769C/cycle-in-maze.cpp
+++ 769C/cycle-in-maze.cpp
@@ -1,31 +1,35 @@
-// MLE
+// TLE
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, k, sr, sc;
+int n, m, k;
 vector<vector<char>> g;
-string ans;
 
-void dfs(string s, int r, int c) {
-  if (s.size() > k) {
-    return;
+string bfs(int sr, int sc) {
+  queue<tuple<string, int, int>> q;
+  q.push({"", sr, sc});
+  while (!q.empty()) {
+    auto [p, r, c] = q.front(); q.pop();
+    if (p.size() > k) {
+      continue;
+    }
+    if (p.size() == k and r == sr and c == sc) {
+      return p;
+    }
+    if (r + 1 < n and g[r + 1][c] == '.') {
+      q.push({p + "D", r + 1, c});
+    }
+    if (c - 1 >= 0 and g[r][c - 1] == '.') {
+      q.push({p + "L", r, c - 1});
+    }
+    if (c + 1 <= m and g[r][c + 1] == '.') {
+      q.push({p + "R", r, c + 1});
+    }
+    if (r - 1 >= 0 and g[r - 1][c] == '.') {
+      q.push({p + "U", r - 1, c});
+    }
   }
-  if (ans.size() == 0 and s.size() == k and r == sr and c == sc) {
-    ans = s;
-    return;
-  }
-  if (r + 1 < n and g[r + 1][c] == '.') {
-    dfs(s + "D", r + 1, c);
-  }
-  if (c - 1 >= 0 and g[r][c - 1] == '.') {
-    dfs(s + "L", r, c - 1);
-  }
-  if (c + 1 < m and g[r][c + 1] == '.') {
-    dfs(s + "R", r, c + 1);
-  }
-  if (r - 1 >= 0 and g[r - 1][c] == '.') {
-    dfs(s + "U", r - 1, c);
-  }
+  return "";
 }
 
 int main() {
@@ -33,6 +37,7 @@ int main() {
   cin.tie(nullptr);
   cin >> n >> m >> k;
   g.resize(n, vector<char>(m));
+  int sr, sc;
   for (int r = 0; r < n; r++) {
     for (int c = 0; c < m; c++) {
       cin >> g[r][c];
@@ -42,8 +47,8 @@ int main() {
       }
     }
   }
-  dfs("", sr, sc);
-  if (k % 2 == 1 or ans.size() == 0) {
+  string ans = bfs(sr, sc);
+  if (k % 2 == 1 or ans.size() != k) {
     cout << "IMPOSSIBLE" << endl;
   } else {
     cout << ans << endl;
