c7aa27dcb4b78d9d4db64d13bf4c1f462fe212e9
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-06-13 08:46:07 -0400

    Update polycarps-pockets.cpp

--- 1003A/polycarps-pockets.cpp
+++ 1003A/polycarps-pockets.cpp
@@ -7,11 +7,11 @@ int main() {
   vector<int> a(100);
   int ans = 0;
   for (int i = 0; i < n; i++) {
-    int id;
-    cin >> id;
-    a[id - 1]++;
-    ans = max(ans, a[id - 1]);
+    int j;
+    cin >> j;
+    a[j - 1]++;
+    ans = max(ans, a[j - 1]);
   }
-  cout << ans << endl;
+  cout << ans << "\n";
   return 0;
 }
