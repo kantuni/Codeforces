de3f34adbc6cac18eed8939528a0117c1d9bd21d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2023-03-02 02:18:07 -0500

    Update romaji.cpp

--- 1008A/romaji.cpp
+++ 1008A/romaji.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isConsonant(char c) {
+bool is_consonant(char c) {
   return c != 'a' and c != 'o' and c != 'u' and c != 'i' and c != 'e';
 }
 
@@ -9,21 +9,21 @@ int main() {
   string s;
   cin >> s;
   char last = s.back();
-  if (isConsonant(last) and last != 'n') {
-    cout << "NO" << endl;
+  if (is_consonant(last) and last != 'n') {
+    cout << "NO" << "\n";
     return 0;
   }
   bool ok = true;
   for (int i = 0; ok and i < s.size() - 1; i++) {
-    if (s[i] != 'n' and isConsonant(s[i]) and isConsonant(s[i + 1])) {
+    if (s[i] != 'n' and is_consonant(s[i]) and is_consonant(s[i + 1])) {
       ok = false;
       break;
     }
   }
   if (ok) {
-    cout << "YES" << endl;
+    cout << "YES" << "\n";
   } else {
-    cout << "NO" << endl;
+    cout << "NO" << "\n";
   }
   return 0;
 }
