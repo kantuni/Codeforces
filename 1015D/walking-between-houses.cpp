63e4a3ae4d48c2fec62b50d1e89d6f5a168bd6e6
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2023-08-10 00:45:30 -0400

    Update walking-between-houses.cpp

--- 1015D/walking-between-houses.cpp
+++ 1015D/walking-between-houses.cpp
@@ -5,17 +5,17 @@ int main() {
   long long n, k, s;
   cin >> n >> k >> s;
   if (k * (n - 1) < s) {
-    cout << "NO" << endl;
+    cout << "NO" << "\n";
     return 0;
   }
   if (s < k) {
-    cout << "NO" << endl;
+    cout << "NO" << "\n";
     return 0;
   }
-  cout << "YES" << endl;
+  cout << "YES" << "\n";
   long long q = s / k;
   long long r = s % k;
-  long long curr = 1;
+  long long cur = 1;
   for (int i = 0; i < k; i++) {
     int step = q;
     if (r > 0) {
@@ -23,12 +23,12 @@ int main() {
       r--;
     }
     if (i % 2 == 0) {
-      curr += step;
+      cur += step;
     } else {
-      curr -= step;
+      cur -= step;
     }
-    cout << curr << " ";
+    cout << cur << " ";
   }
-  cout << endl;
+  cout << "\n";
   return 0;
 }
