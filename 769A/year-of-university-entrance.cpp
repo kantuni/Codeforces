7e577ed2dde728f3a8c224a1a2fa50081d0d7ba7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2024-03-15 15:31:42 -0400

    769A: Accepted

---
+++ 769A/year-of-university-entrance.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  cout << a[n / 2] << "\n";
+  return 0;
+}
