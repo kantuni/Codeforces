061fdb863a18db48e9b64ec4f59e4e3f86797385
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2024-03-23 13:37:19 -0400

    769D: TLE, this time on 28

--- 769D/k-interesting-pairs-of-integers.cpp
+++ 769D/k-interesting-pairs-of-integers.cpp
@@ -1,43 +1,35 @@
-// TLE on 24
+// TLE on 28
 #include <bits/stdc++.h>
 #define N 14
 using namespace std;
 
-map<tuple<int, int, int>, set<int>> memo;
+map<tuple<int, int, int>, vector<int>> memo;
 
-set<int> interesting(int x, int k, int xm, int j) {
-  // pruning
-  if (x > 10000) {
-    return {};
-  }
-  tuple<int, int, int> key = {x, k, xm};
+vector<int> interesting(int x, int k, int i) {
+  tuple<int, int, int> key = {x, k, i};
   if (memo.count(key) > 0) {
     return memo[key];
   }
   if (k == 0) {
-    memo[key] = {x};
     return {x};
   }
-  set<int> res;
-  bitset<N> bsxm(xm);
-  for (int i = j; i < N; i++) {
-    // pruning
-    if (N - i < k) {
-      break;
-    }
-    bitset<N> bsx(x);
-    if (bsxm[i] == 0) {
-      bsx.flip(i);
-      bsxm.flip(i);
-      int y = bsx.to_ulong();
-      int ym = bsxm.to_ulong();
-      auto rest = interesting(y, k - 1, ym, j + 1);
-      for (auto num: rest) {
-        res.insert(num);
-      }
-      bsx.flip(i);
-      bsxm.flip(i);
-    }
+  if (i == N) {
+    return {};
+  }
+  if (N - i < k) {
+    return {};
+  }
+  vector<int> res;
+  bitset<N> bsx(x);
+  auto c1 = interesting(x, k, i + 1);
+  for (auto num: c1) {
+    res.push_back(num);
+  }
+  bsx.flip(i);
+  int y = bsx.to_ulong();
+  auto c2 = interesting(y, k - 1, i + 1); 
+  for (auto num: c2) {
+    res.push_back(num);
   }
   memo[key] = res;
   return res;
@@ -55,7 +47,7 @@ int main() {
   map<int, int> freq;
   long long ans = 0;
   for (int i = n - 1; i > -1; i--) {
-    auto kis = interesting(a[i], k, 0, 0);
+    auto kis = interesting(a[i], k, 0);
     for (auto num: kis) {
       if (freq[num] > 0) {
         ans += freq[num];
