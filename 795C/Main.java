eeea663a0be506555eae0c58c3b9d89183ed5e3b
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2024-04-05 14:52:09 -0400

    795C: Simpler solution

--- 795C/Main.java
+++ 795C/Main.java
@@ -1,24 +1,18 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-    try {
-      int n = Integer.parseInt(buffer.readLine().trim());
-      StringBuilder answer = new StringBuilder();
-      if (n % 2 == 1) {
-        answer.append(7);
-        n -= 3;
-      }
-      while (n - 2 >= 0) {
-        answer.append(1);
-        n -= 2;
-      }
-      System.out.println(answer.toString());
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      e.printStackTrace();
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    StringBuilder answer = new StringBuilder();
+    if (n % 2 == 1) {
+      answer.append(7);
+      n -= 3;
     }
+    while (n > 0) {
+      answer.append(1);
+      n -= 2;
+    }
+    System.out.println(answer.toString());
   }
 }
