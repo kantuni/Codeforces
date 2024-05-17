c09f7df7397bbf98565b007bf9eedcdd05459c8b
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2024-05-16 22:52:22 -0400

    820C: Wrong answer

--- 820C/mister-b-and-boring-game.cpp
+++ 820C/mister-b-and-boring-game.cpp
@@ -1,57 +1,58 @@
-// MLE
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-  int a, b;
-  long long l, r;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int a, b, l, r;
   cin >> a >> b >> l >> r;
-  if (l == r) {
-    cout << 1 << endl;
-    return 0;
-  }
   string s;
+  // a
   for (int i = 0; i < a; i++) {
     s += 'a' + i;
   }
-  int turn = 0;
-  while (s.size() < r) {
-    if (turn % 2 == 0) {
-      if (s.size() < l - 1) {
-        for (int i = s.size() - a; i < s.size(); i++) {
-          s += s[i];
-        }
-      } else {
-        for (int i = 0; i < a; i++) {
-          s += s[l - 1];
-        }
-      }
+  // a + b
+  for (int i = 0; i < b; i++) {
+    s += s.back();
+  }
+  // a + b + a
+  set<char> sa;
+  for (int i = 0; i < a; i++) {
+    sa.insert(s[s.size() - 1 - i]);
+  }
+  int ac = a;
+  for (int i = 0; i < ac; i++) {
+    char c = 'a' + i;
+    if (sa.count(c) == 0) {
+      s += c;
     } else {
-      string suffix = "";
-      for (int i = s.size() - b; i < s.size(); i++) {
-        suffix += s[i];
-      }
-      vector<bool> lex(26, true);
-      for (int i = 0; i < suffix.size(); i++) {
-        int l = (int) suffix[i] - 97;
-        lex[l] = false;
-      }
-      string t = "";
-      for (int i = 0; i < lex.size(); i++) {
-        if (lex[i] and t.size() < a) {
-          t += 'a' + i;
-        }
-      }
-      s += t;
+      ac++;
     }
-    turn++;
   }
-  string ans;
-  for (int i = l - 1; i < r; i++) {
-    ans += s[i];
+  // a + b + a + b
+  for (int i = 0; i < b; i++) {
+    s += s.back();
   }
-  sort(begin(ans), end(ans));
-  ans.erase(unique(begin(ans), end(ans)), end(ans));
-  cout << ans.size() << endl;
+  int len = 2 * (a + b);
+  int ans;
+  if (r - l + 1 < len) {
+    l %= len;
+    r %= len;
+    set<char> cs;
+    // `i != r` instead of `i < r` as i can become larger than r before %.
+    // Example: i = 29, r = 20, len = 32
+    //          ^ has to do a rotation to reach r
+    for (int i = l; i != (r + 1) % len; i = (i + 1) % len) {
+      // l and r are 1-based indexes
+      int im = (i - 1 + len) % len;
+      cs.insert(s[im]);
+    }
+    ans = cs.size();
+  } else {
+    // A cycle has formed. No new letters will be added. 
+    set<char> cs(s.begin(), s.end());
+    ans = cs.size();
+  }
+  cout << ans << "\n";
   return 0;
 }
