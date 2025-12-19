fe8e376c5d39ca49789461d640990727031f871e
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2025-12-19 06:16:47 -0500

    1202A: Accepted
    Refactor binary string addition and optimize logic.

--- 1202A/you-are-given-two-binary-strings.cpp
+++ 1202A/you-are-given-two-binary-strings.cpp
@@ -1,19 +1,5 @@
-// TLE
 #include <bits/stdc++.h>
 using namespace std;
-
-string add(string& a, string& b) {
-  string ans;
-  int i = a.size() - 1, j = b.size() - 1;
-  int carry = 0;
-  while (i > -1 || j > -1 || carry == 1) {
-    carry += (i > -1) ? a[i--] - '0' : 0;
-    carry += (j > -1) ? b[j--] - '0' : 0;
-    ans = char(carry % 2 + '0') + ans;
-    carry /= 2;
-  }
-  return ans;
-}
 
 int main() {
   ios::sync_with_stdio(false);
@@ -23,19 +9,24 @@ int main() {
   while (t--) {
     string x, y;
     cin >> x >> y;
-    string mns = add(x, y);
-    reverse(mns.begin(), mns.end());
-    int ans = 0;
-    for (int k = 1; k < 17; k++) {
-      string y2k = y + string(k, '0');
-      string sk = add(x, y2k);
-      reverse(sk.begin(), sk.end());
-      if (sk < mns) {
-        mns = sk;
-        ans = k;
+    int skip = 0;
+    for (int i = y.size() - 1; i > -1; i--) {
+      if (y[i] == '1') {
+        break;
       }
+      skip++;
     }
-    cout << ans << endl;
+    for (int i = 0; i < skip; i++) {
+      x.pop_back();
+    }
+    int k = 0;
+    for (int j = x.size() - 1; j > -1; j--) {
+      if (x[j] == '1') {
+        break;
+      }
+      k++;
+    }
+    cout << k << "\n";
   }
   return 0;
 }
